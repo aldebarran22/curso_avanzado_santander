30cf1402e3fa83423448787c7015e6af7ebb2c3c
aldebarran22 <antonioespinherranz@hotmail.com>
2024-01-29 09:51:41 +0100

    9.51

---
+++ codigo_ene_29_feb_2/repaso.py
@@ -0,0 +1,18 @@
+"""
+Repaso de tipos en Python
+
+Uso de funciones:
+L = [1,2,3]
+len(L)
+
+L.sort()
+"""
+
+def diccionario():
+    cad = "hola"
+    L = [1,2,3,4]
+    d = dict(zip(L, cad))
+    print(d)
+
+if __name__ == '__main__':
+    diccionario()
