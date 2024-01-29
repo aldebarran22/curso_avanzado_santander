58b621ac7c005be19312534a2727ab401661a11b
aldebarran22 <antonioespinherranz@hotmail.com>
2024-01-29 10:01:09 +0100

    10.00

--- codigo_ene_29_feb_2/repaso.py
+++ codigo_ene_29_feb_2/repaso.py
@@ -14,5 +14,35 @@ def diccionario():
     d = dict(zip(L, cad))
     print(d)
 
+def copiarObjetosMutables():
+    # Ojo crea una ref.
+    L = [1,2,3,4]
+    L2 = L
+    L[0] = 1000
+    print('L',L, id(L))
+    print('L2', L2, id(L2))
+
+    # Copiar pero con elementos inmutables:
+    L = [1,2,3,4]
+    L2 = L.copy()
+    L[0] = 1000
+    print('L',L, id(L))
+    print('L2', L2, id(L2))
+    print()
+
+    # Copiar pero con elementos mutables:
+    L = [[1,2],[3,4]]
+    L2 = L.copy()
+    L[0] = 1000
+    L[-1].append(999)
+    print('L',L, id(L))
+    print('L2', L2, id(L2))
+    print()
+
+
+
+
+
 if __name__ == '__main__':
-    diccionario()
+    #diccionario()
+    copiarObjetosMutables()
