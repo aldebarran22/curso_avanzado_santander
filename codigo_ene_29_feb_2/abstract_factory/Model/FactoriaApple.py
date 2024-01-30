9f12ece5df56d2348d08b0ca1951c784bddb23ba
aldebarran22 <antonioespinherranz@hotmail.com>
2024-01-30 10:45:06 +0100

    10.45

--- codigo_ene_29_feb_2/abstract_factory/Model/FactoriaApple.py
+++ codigo_ene_29_feb_2/abstract_factory/Model/FactoriaApple.py
@@ -1,15 +1,21 @@
 #!/usr/bin/python
-#-*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 
 from Factoria import Factoria
+from IPad import IPad
+from IPhone import IPhone
+
 
 class FactoriaApple(Factoria):
     def __init__(self):
         pass
 
-    def createTablet(self, ):
-        pass
+    def createTablet(
+        self,
+    ):
+        return IPad()
 
-    def createTno(self, ):
-        pass
-
+    def createTno(
+        self,
+    ):
+        return IPhone()
