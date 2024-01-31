46c4d431d8f2a491818dcb09fbd371a3c82ca91a
aldebarran22 <antonioespinherranz@hotmail.com>
2024-01-31 13:02:15 +0100

    13.02

---
+++ codigo_ene_29_feb_2/pandas_names.py
@@ -0,0 +1,22 @@
+"""
+1) Operar con dos DataFrames
+2) Obtener el TOP 5 de los nombres más utilizados (F y M)
+en un rango de años.
+"""
+import pandas as pd
+from pandas import DataFrame
+
+
+def cargarDF(año):
+    """Carga y devuelve un DataFrame"""
+    pass
+
+
+def sumarDosAños(año1, año2):
+    """Suma dos DataFrames con el operador +"""
+    pass
+
+
+if __name__ == "__main__":
+    dfSuma = sumarDosAños(1970, 1971)
+    print(dfSuma.head(6))
