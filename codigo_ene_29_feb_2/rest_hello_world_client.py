6a6aa28c015c52dd4b7280343a35c89ef9b66a56
aldebarran22 <antonioespinherranz@hotmail.com>
2024-02-02 12:12:13 +0100

    12.12

---
+++ codigo_ene_29_feb_2/rest_hello_world_client.py
@@ -0,0 +1,9 @@
+"""
+Ejemplo de petición GET a un servicio de REST con la librería 
+requests
+"""
+import requests
+
+if __name__ == '__main__':
+    rep = requests.get('http://localhost:5000')
+    print(rep.json())
