c2f70a4c0602fa553235a1cd5b9130b3933889b4
aldebarran22 <antonioespinherranz@hotmail.com>
2024-02-02 09:25:26 +0100

    9.25

---
+++ codigo_ene_29_feb_2/exportaciones_json_xml.py
@@ -0,0 +1,22 @@
+"""
+Obtener información de la Base de datos en json / xml
+"""
+
+import json
+from base_datos import Categoria, Producto, BaseDatos
+
+
+def exportarXML():
+    pass
+
+
+def exportarJSON():
+    pass
+
+
+def testBD():
+    pass
+
+
+if __name__ == "__main__":
+    testBD()
