8cee18f4b6a1c46a8f15d90a40abbb0b5cee5459
aldebarran22 <antonioespinherranz@hotmail.com>
2024-02-02 12:41:43 +0100

    12.41

--- codigo_ene_29_feb_2/rest_base_datos_server.py
+++ codigo_ene_29_feb_2/rest_base_datos_server.py
@@ -8,21 +8,26 @@ D -> Delete -> Verbo DELETE (eliminar un recurso)
 """
 
 from flask import Flask
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, abort
 from base_datos import Empleado, Producto, Categoria, BaseDatos
 
 # Creamos la aplicación y el Api
 app = Flask(__name__)
 api = Api(app)
 
+path = "../../BBDD/empresa3.db"
+
 
 class ProductosBD(Resource):
     def get(self):
         # Recuperar todos los productos
         try:
-            pass
+            bd = BaseDatos(path)
+            L = bd.select()
+            return [p.to_json() for p in L]
+
         except Exception as e:
-            pass
+            abort(404, message=str(e))
 
 
 # Mapeo:
