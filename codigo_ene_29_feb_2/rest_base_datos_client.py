4db6235a8efcab1289ea8c8d34f523f16ea3cda0
aldebarran22 <antonioespinherranz@hotmail.com>
2024-02-02 12:49:27 +0100

    12.49

---
+++ codigo_ene_29_feb_2/rest_base_datos_client.py
@@ -0,0 +1,18 @@
+"""
+Ejemplo de peticiones GET/DELETE/PUT/POST a un servicio de REST 
+con la librería requests
+"""
+import requests
+import json
+
+def testGET():
+    rep = requests.get("http://localhost:5000/productos")
+    datos = rep.json()
+    #print(datos, type(datos))
+    with open("productos_rest.json", "w", encoding="utf8") as f:
+        json.dump(datos, f)
+        print('Fichero generado ...')
+
+
+if __name__ == "__main__":
+    testGET()
