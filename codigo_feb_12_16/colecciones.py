e5d608a560b6a002ade641d4e67850a89d65e135
aldebarran22 <antonioespinherranz@hotmail.com>
2024-02-12 09:50:58 +0100

    9.50

--- codigo_feb_12_16/colecciones.py
+++ codigo_feb_12_16/colecciones.py
@@ -5,7 +5,16 @@ Repaso de colecciones.
 
 def csvToDict(path):
     """Genera una lista de diccionarios"""
-    pass
+    fich = None
+    try:
+        fich = open(path, "r")
+        csv = fich.read()
+        print(csv)
+    except Exception as e:
+        print(e.__class__.__name__, e)
+    finally:
+        if fich:
+            fich.close()
 
 
 def dictToCSV(d):
