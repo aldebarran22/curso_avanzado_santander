875ac4127d7837802a9951b094bfbecdb7e3c567
aldebarran22 <antonioespinherranz@hotmail.com>
2024-02-12 11:03:40 +0100

    11.03

--- codigo_feb_12_16/objetos.py
+++ codigo_feb_12_16/objetos.py
@@ -24,25 +24,30 @@ class Persona:
             # f-string
             print(f"{self.nombre} y {otro.nombre} están hablando")
 
-    """
     def __str__(self):
         return self.nombre + " " + str(self.edad) + " " + str(self.altura)
 
     def __repr__(self):
         return str(self)
 
+    """
     def __lt__(self, other):
         return self.edad < other.edad
     """
 
     def __del__(self):
-        # print('se borra a ', self.nombre)
-        pass
+        print("se borra a ", self.nombre)
+        # pass
 
 
 def testPersona():
     p1 = Persona("Ana", 23, 1.77)
     print(p1)
+    # print(str(p1))
+    # print(p1.__str__())
+    L = [p1, Persona("Jorge", 44, 1.8), Persona("Raúl", 55, 1.68)]
+    print(L)
+    
 
 
 if __name__ == "__main__":
