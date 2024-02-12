207a1b3b5702ddf90c59d2c2376f1dc71650bbb3
aldebarran22 <antonioespinherranz@hotmail.com>
2024-02-12 12:34:25 +0100

    12.34

--- codigo_feb_12_16/fecha_hora.py
+++ codigo_feb_12_16/fecha_hora.py
@@ -5,6 +5,9 @@ class Time:
         self.m = mm
         self.s = ss
 
+    def __add__(self, other):
+        pass
+
     def __str__(self):
         return "%02d:%02d:%02d" % (self.h, self.m, self.s)
 
@@ -30,13 +33,18 @@ class Date:
 class Datetime(Time, Date):
     """Imprime con el formato: 12/02/2024 12:17:07"""
 
-    def __init__(self,dd=1, mm=1, yy=1970, h=0, m=0, s=0):
-        Date.__init__(self,dd, mm, yy)
-        Time.__init__(self, h,m,s)
+    def __init__(self, dd=1, mm=1, yy=1970, h=0, m=0, s=0):
+        Date.__init__(self, dd, mm, yy)
+        Time.__init__(self, h, m, s)
 
     def __str__(self):
-        return Date.__str__(self)+ " "+Time.__str__(self)
+        return Date.__str__(self) + " " + Time.__str__(self)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     dt = Datetime(h=15)
     print(dt)
+
+    t1 = Time(2, 33, 45)
+    t2 = Time(4, 46, 12)
+    r = t1 + t2  # r = t1.__add__(t2)
