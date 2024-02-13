5b7c3374f86d4d42f2b6920c58c87eb5c2eda507
aldebarran22 <antonioespinherranz@hotmail.com>
2024-02-13 13:46:21 +0100

    1346

--- codigo_feb_12_16/patron_cadena_responsabilidad.py
+++ codigo_feb_12_16/patron_cadena_responsabilidad.py
@@ -4,6 +4,7 @@ Transmitir una petición a través de la cadena
 Se la envíamos al primer objeto y si es para él
 la trata, si no la envía al siguiente de la cadena.
 """
+
 import abc
 
 
@@ -21,19 +22,53 @@ class Manejador(abc.ABC):
     def procesarPeticion(self, peticion):
         pass
 
+
 class ManejadorEMail(Manejador):
     def __init__(self, siguiente=None):
         Manejador.__init__(self, siguiente)
 
     def procesarPeticion(self, peticion):
-        if peticion.destino == 'email':
+        if peticion.destino == "email":
             # Utilizar un API de email
-            print('Enviar por email: ', peticion.contenido)
+            print("Enviar por email: ", peticion.contenido)
 
         elif self.siguiente != None:
-            print('Email transmite la petición al siguiente')
+            print("Email transmite la petición al siguiente")
             self.siguiente.procesarPeticion(peticion)
 
         else:
-            print('fin de cadena, no se procesar la petición')
+            print("fin de cadena, no se procesar la petición")
 
+
+class ManejadorSMS(Manejador):
+    def __init__(self, siguiente=None):
+        Manejador.__init__(self, siguiente)
+
+    def procesarPeticion(self, peticion):
+        if peticion.destino == "sms":
+            # Utilizar un API de SMS
+            print("Enviar por SMS: ", peticion.contenido)
+
+        elif self.siguiente != None:
+            print("SMS transmite la petición al siguiente")
+            self.siguiente.procesarPeticion(peticion)
+
+        else:
+            print("fin de cadena, no se procesar la petición")
+
+
+class ManejadorWhatsApp(Manejador):
+    def __init__(self, siguiente=None):
+        Manejador.__init__(self, siguiente)
+
+    def procesarPeticion(self, peticion):
+        if peticion.destino == "whatsapp":
+            # Utilizar un API de WHatsApp
+            print("Enviar por WHatsApp: ", peticion.contenido)
+
+        elif self.siguiente != None:
+            print("WHatsApp transmite la petición al siguiente")
+            self.siguiente.procesarPeticion(peticion)
+
+        else:
+            print("fin de cadena, no se procesar la petición")
