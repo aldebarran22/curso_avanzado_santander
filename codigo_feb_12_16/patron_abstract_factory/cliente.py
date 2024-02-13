4dbccdda36424488ccf1b8fc1c2b6d3fdc000abb
aldebarran22 <antonioespinherranz@hotmail.com>
2024-02-13 10:26:07 +0100

    10.26

--- codigo_feb_12_16/patron_abstract_factory/cliente.py
+++ codigo_feb_12_16/patron_abstract_factory/cliente.py
@@ -4,16 +4,22 @@ from FactoriaSamsung import FactoriaSamsung
 
 
 def seleccionarFabricante():
-    L = Factoria.__subclasses__()
-    print(L)
+    # Mostrar un menú con las factorias disponibles y devolver
+    # la factoria creada que haya elegido el cliente.
+    L = [c for c in Factoria.__subclasses__()]
+    print("MENU")
+    for pos, c in enumerate(L):
+        print(pos + 1, c.__name__)
+    op = int(input("Seleccionar factoria:"))
+    return L[op - 1]()
 
 
 if __name__ == "__main__":
     factory = seleccionarFabricante()
-    """
+    print(factory.__class__)
+
     tno = factory.createTno()
     tablet = factory.createTablet()
 
     tno.llamar()
     tablet.internet()
-    """
