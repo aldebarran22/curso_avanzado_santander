0494ba7dffce2e2aef769e54fb254f412d22bb61
aldebarran22 <antonioespinherranz@hotmail.com>
2024-02-14 14:29:41 +0100

    14.29

--- codigo_feb_12_16/pandas_limpiar_correlacion.py
+++ codigo_feb_12_16/pandas_limpiar_correlacion.py
@@ -6,7 +6,12 @@ import pandas as pd
 
 def limpiarDF(path):
     df = pd.read_csv(path, delimiter=";")
+    df.columns = [col.rstrip() for col in df.columns]
+    df['Lat'] = pd.to_numeric(df.Lat.str[:-1], downcast="float")
+    df['Lon'] = pd.to_numeric(df.Lon.str[:-1], downcast="float")
+    df['Wind'] = pd.to_numeric(df.Wind.str.replace(" mph",""),downcast="integer")
     df.info()
+    
 
 if __name__ == "__main__":
     limpiarDF("pandas_ficheros/IRMA.csv")
