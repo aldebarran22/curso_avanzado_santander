7cb5df301cd889b6e4800e887ff3b8ea2772c3d7
aldebarran22 <antonioespinherranz@hotmail.com>
2024-02-14 11:59:33 +0100

    12.00

---
+++ codigo_feb_12_16/pandas_exportar_paises.py
@@ -0,0 +1,13 @@
+"""
+A partir del fichero: Pedidos.txt
+Generar un fichero CSV para cada país y copiar 
+los pedidos de cada pais
+"""
+
+
+def exportarPaises(pathPedidos):
+    pass
+
+
+if __name__ == "__main__":
+    exportarPaises("pandas_ficheros/Pedidos.txt")
