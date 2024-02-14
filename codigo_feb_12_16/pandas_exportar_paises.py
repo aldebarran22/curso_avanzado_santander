a020441db8b5a03c643054758d6f0cf78c8b81f5
aldebarran22 <antonioespinherranz@hotmail.com>
2024-02-14 12:13:02 +0100

    12.12

--- codigo_feb_12_16/pandas_exportar_paises.py
+++ codigo_feb_12_16/pandas_exportar_paises.py
@@ -3,10 +3,16 @@ A partir del fichero: Pedidos.txt
 Generar un fichero CSV para cada país y copiar 
 los pedidos de cada pais
 """
-
+import pandas as pd
 
 def exportarPaises(pathPedidos):
-    pass
+    df = pd.read_csv(pathPedidos, delimiter=';')
+    print(df.shape)
+    paises = df.pais.unique()
+    for pais in paises:
+        pathPais = f"ficheros/{pais}.csv"
+        print(pathPais)
+        df[df.pais == pais].to_csv(pathPais, index=False, decimal=',',sep=";")
 
 
 if __name__ == "__main__":
