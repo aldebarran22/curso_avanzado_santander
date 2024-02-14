ac8bf3de4d7d86c47340a6e703dd46af6785ba84
aldebarran22 <antonioespinherranz@hotmail.com>
2024-02-14 09:17:58 +0100

    9.17

--- codigo_feb_12_16/patron_adapter.py
+++ codigo_feb_12_16/patron_adapter.py
@@ -1,59 +1,88 @@
 import math, abc
 
-class Vector3D:
-	
-	def __init__(self,x=0,y=0,z=0):
-		self.__x = x
-		self.__y = y
-		self.__z = z
-		
-	def getX(self):
-		return self.__x
-		
-	def getY(self):
-		return self.__y
 
-	def getZ(self):
-		return self.__z
-				
-	def productoEscalar(self, vector):
-		return self.__x * vector.__x + self.__y * vector.__y + self.__z * vector.__z
-		
-	def norma(self):
-		return math.sqrt(self.__x**2+self.__y**2+self.__z**2)
-		
-	def __str__(self):
-		return str(self.__x)+","+str(self.__y)+","+str(self.__z)
-		
-		
+class Vector3D:
+
+    def __init__(self, x=0, y=0, z=0):
+        self.__x = x
+        self.__y = y
+        self.__z = z
+
+    def getX(self):
+        return self.__x
+
+    def getY(self):
+        return self.__y
+
+    def getZ(self):
+        return self.__z
+
+    def productoEscalar(self, vector):
+        return self.__x * vector.__x + self.__y * vector.__y + self.__z * vector.__z
+
+    def norma(self):
+        return math.sqrt(self.__x**2 + self.__y**2 + self.__z**2)
+
+    def __str__(self):
+        return str(self.__x) + "," + str(self.__y) + "," + str(self.__z)
+
+
 class Vector2D(abc.ABC):
-	
-	@abc.abstractmethod
-	def getAbcisa(self): 
-		pass
-	
-	@abc.abstractmethod	
-	def getOrdenada(self):
-		pass
-		
-	@abc.abstractmethod
-	def prod(self, v):
-		pass
-		
-	@abc.abstractmethod
-	def magnitud(self):
-		# La norma del vector
-		pass
+
+    @abc.abstractmethod
+    def getAbcisa(self):
+        pass
+
+    @abc.abstractmethod
+    def getOrdenada(self):
+        pass
+
+    @abc.abstractmethod
+    def prod(self, v):
+        pass
+
+    @abc.abstractmethod
+    def magnitud(self):
+        # La norma del vector
+        pass
 
 
 class VectorPlano1(Vector3D, Vector2D):
-	"""Solución por herencia múltiple"""
-	
-	def __init__(self,x=0,y=0):
-		Vector3D.__init__(self,x,y)
+    """Solución por herencia múltiple"""
+
+    def __init__(self, x=0, y=0):
+        Vector3D.__init__(self, x, y)
+
+    def getAbcisa(self):
+        return Vector3D.getX(self)
+        # return self.getX()
+
+    def getOrdenada(self):
+        return Vector3D.getY(self)
+
+    def prod(self, v):
+        pass
+
+    def magnitud(self):
+        # La norma del vector
+        pass
+
 
 class VectorPlano2(Vector2D):
-	"""Solución por composición"""
+    """Solución por composición"""
 
-	def __init__(self,x=0,y=0):
-		self.vector3D = Vector3D(x,y)
+    def __init__(self, x=0, y=0):
+        self.vector3D = Vector3D(x, y)
+
+    def getAbcisa(self):
+        return self.vector3D.getX()
+
+    def getOrdenada(self):
+        return self.vector3D.getY()
+
+    def prod(self, v):
+        pass
+
+    def magnitud(self):
+        # La norma del vector
+        pass
