15bc237a92ae18a80dd371ab8ee3d5efea9abc23
aldebarran22 <antonioespinherranz@hotmail.com>
2024-02-14 14:39:13 +0100

    14.39

--- codigo_feb_12_16/pandas_limpiar_correlacion.py
+++ codigo_feb_12_16/pandas_limpiar_correlacion.py
@@ -2,16 +2,25 @@
 Limpiar un fichero CSV y estudiar la correlación
 entre dos columnas: Viento y la Presión
 """
+
 import pandas as pd
+
 
 def limpiarDF(path):
     df = pd.read_csv(path, delimiter=";")
     df.columns = [col.rstrip() for col in df.columns]
-    df['Lat'] = pd.to_numeric(df.Lat.str[:-1], downcast="float")
-    df['Lon'] = pd.to_numeric(df.Lon.str[:-1], downcast="float")
-    df['Wind'] = pd.to_numeric(df.Wind.str.replace(" mph",""),downcast="integer")
+    df["Lat"] = pd.to_numeric(df.Lat.str[:-1], downcast="float")
+    df["Lon"] = pd.to_numeric(df.Lon.str[:-1], downcast="float")
+    df["Wind"] = pd.to_numeric(df.Wind.str.replace(" mph", ""), downcast="integer")
+    df["Pressure"] = pd.to_numeric(
+        df.Pressure.str.replace(" mb", ""), downcast="integer"
+    )
+    df["DateTime"] = pd.to_datetime(
+        "2005 " + df.Date + " " + df.Time.str.replace(" GMT", ""),
+        infer_datetime_format=True,
+    )
     df.info()
-    
+
 
 if __name__ == "__main__":
     limpiarDF("pandas_ficheros/IRMA.csv")
