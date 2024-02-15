b024055958e4452bf482cefa8a3ae77be7041f2b
aldebarran22 <antonioespinherranz@hotmail.com>
2024-02-15 13:14:40 +0100

    13.14

--- codigo_feb_12_16/exportaciones.py
+++ codigo_feb_12_16/exportaciones.py
@@ -2,8 +2,9 @@
 Exportar a JSON / XML empleados de la BD
 """
 
-from basedatosCRUD import BaseDatos
+from basedatosCRUD import BaseDatos, Empleado
 import json
+
 
 def exportarJSON(bd, path):
     fich = None
@@ -20,6 +21,25 @@ def exportarJSON(bd, path):
             fich.close()
 
 
+def importarJSON(path):
+    fich = None
+    try:
+        fich = open(path, "r")
+        # Restaurar una lista de dict de Python
+        L = json.load(fich)
+        # Pasar de la lista de dict a una lista de objetos Empleado
+        empleados = [Empleado(**d) for d in L]
+        return empleados
+
+    except Exception as e:
+        print(e)
+    finally:
+        if fich:
+            fich.close()
+
+
 if __name__ == "__main__":
     bd = BaseDatos("../BBDD/empresa3.db")
     exportarJSON(bd, "ficheros/empleados.json")
+    L = importarJSON("ficheros/empleados.json")
+    print(L[:3])
