f163733313e97ea823fbeb5803f52d7483ff6dda
aldebarran22 <antonioespinherranz@hotmail.com>
2024-02-15 13:05:17 +0100

    1305

--- codigo_feb_12_16/exportaciones.py
+++ codigo_feb_12_16/exportaciones.py
@@ -3,10 +3,21 @@ Exportar a JSON / XML empleados de la BD
 """
 
 from basedatosCRUD import BaseDatos
-
+import json
 
 def exportarJSON(bd, path):
-    pass
+    fich = None
+    try:
+        fich = open(path, "w")
+        empleados = bd.select()
+        L = [emp.__dict__ for emp in empleados]
+        json.dump(L, fich, indent=4)
+
+    except Exception as e:
+        print(e)
+    finally:
+        if fich:
+            fich.close()
 
 
 if __name__ == "__main__":
