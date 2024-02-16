53c3e4fb1b8c4cec769ea5fa41a9ea4dc5a97c4c
aldebarran22 <antonioespinherranz@hotmail.com>
2024-02-16 12:19:15 +0100

    12.19

---
+++ codigo_feb_12_16/clientesREST.py
@@ -0,0 +1,12 @@
+"""
+Consumir servicios REST con la librería requests
+"""
+from requests import get
+
+def testHello():
+    url = "http://localhost:5000"
+    resp = get(url)
+    print(resp.json())
+
+if __name__ == '__main__':
+    testHello()
