e2eb751d4e141cbcbc6f4bfb75290df3d6dbfd37
aldebarran22 <antonioespinherranz@hotmail.com>
2024-02-16 14:48:38 +0100

    14.48

--- codigo_feb_12_16/patron_Fachada.py
+++ codigo_feb_12_16/patron_Fachada.py
@@ -12,11 +12,24 @@ class C:
     # Atajo a la función strchr de C
     __strchr = __libc.strchr
 
+    # Atajo a la función strstr de C
+    __strstr = __libc.strstr
+
     @staticmethod
     def strchr(cadena, caracter):
         # Registrar el tipo devuelto:
         C.__strchr.restype = c_char_p
         resul = C.__strchr(cadena.encode("utf8"), ord(caracter))
+        if resul:
+            return resul.decode("utf8")
+        else:
+            return resul
+
+    @staticmethod
+    def strstr(cadena, subcadena):
+        # Registrar el tipo devuelto:
+        C.__strstr.restype = c_char_p
+        resul = C.__strstr(cadena.encode("utf8"), subcadena.encode("utf8"))
         if resul:
             return resul.decode("utf8")
         else:
@@ -28,4 +41,7 @@ if __name__ == "__main__":
     print(resul)
 
     resul = C.strchr("hola que tal", "X")
+    print(resul)
+
+    resul = C.strstr("hola que tal", "que")
     print(resul)
