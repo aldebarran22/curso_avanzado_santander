0fc2f71e843b6310d2828368a10cb1a2f4018a51
aldebarran22 <antonioespinherranz@hotmail.com>
2024-02-26 12:23:00 +0100

    12.22

--- codigo_feb_26_mar_1/objetos.py
+++ codigo_feb_26_mar_1/objetos.py
@@ -18,9 +18,19 @@ class Persona:
         self.altura = altura
         Persona.contador += 1
 
+    def __str__(self):
+        return self.nombre+" "+str(self.edad)+" "+str(self.altura)
+
     def __del__(self):
         print("Se elimina a ", self.nombre)
         Persona.contador -= 1
 
 if __name__ == '__main__':
-    
+    p1 = Persona("Ana",33, 1.8)
+    p2 = Persona("Marcos",36, 1.7)
+    del(p2)
+    print('continua...')
+    print(p1)
+    print(str(p1))
+    print(p1.__str__())
+
