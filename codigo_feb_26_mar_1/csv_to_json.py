69c440721e354df3048f18a059d52374ea5352b3
aldebarran22 <antonioespinherranz@hotmail.com>
2024-02-26 10:22:53 +0100

    10.22

--- codigo_feb_26_mar_1/csv_to_json.py
+++ codigo_feb_26_mar_1/csv_to_json.py
@@ -3,7 +3,7 @@ Convertir un formato CSV a json, y de json a csv
 """
 
 
-def cargaFich(path, sep=";"):
+def cargaFich(path):
     fich=None
     try:
         fich = open(path, "r")
@@ -17,9 +17,11 @@ def cargaFich(path, sep=";"):
         
 
 
-def csvToJson(txt):
-    pass
-
+def csvToJson(txt, sep=";"):
+    filas = txt.split("\n")
+    cabs = filas[0].split(sep)
+    L =[dict(zip(cabs, fila.split(sep))) for fila in filas[1:]]
+    return L
 
 def jsonToCSV(L):
     pass
@@ -27,8 +29,12 @@ def jsonToCSV(L):
 
 if __name__ == "__main__":
     txt = cargaFich("../ficheros/Empleados.txt")
-
+    L = csvToJson(txt)
+    for i in L:
+        print(i)
+    """
     col1 = "id;nombre;cargo".split(";")
     col2 = "1;Davolio;Representante de ventas".split(";")
     d = dict(zip(col1, col2))
     print(d)
+    """
