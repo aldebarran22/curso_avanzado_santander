b6aed485bce714254a3d013c26ca0a2f8964add6
aldebarran22 <antonioespinherranz@hotmail.com>
2024-02-26 10:29:57 +0100

    10.29

--- codigo_feb_26_mar_1/csv_to_json.py
+++ codigo_feb_26_mar_1/csv_to_json.py
@@ -4,7 +4,7 @@ Convertir un formato CSV a json, y de json a csv
 
 
 def cargaFich(path):
-    fich=None
+    fich = None
     try:
         fich = open(path, "r")
         txt = fich.read()
@@ -13,18 +13,23 @@ def cargaFich(path):
     except IOError as e:
         print(e)
     finally:
-        if fich:fich.close()
-        
+        if fich:
+            fich.close()
 
 
 def csvToJson(txt, sep=";"):
     filas = txt.split("\n")
     cabs = filas[0].split(sep)
-    L =[dict(zip(cabs, fila.split(sep))) for fila in filas[1:]]
+    L = [dict(zip(cabs, fila.split(sep))) for fila in filas[1:] if len(fila) > 1]
     return L
 
-def jsonToCSV(L):
-    pass
+
+def jsonToCSV(L, sep=";"):
+    cabs = sep.join(L[0].keys())
+    filas = [cabs]
+    for d in L:
+        filas.append(sep.join(d.values()))
+    return "\n".join(filas)
 
 
 if __name__ == "__main__":
@@ -32,6 +37,8 @@ if __name__ == "__main__":
     L = csvToJson(txt)
     for i in L:
         print(i)
+    csv = jsonToCSV(L)
+    print(csv)
     """
     col1 = "id;nombre;cargo".split(";")
     col2 = "1;Davolio;Representante de ventas".split(";")
