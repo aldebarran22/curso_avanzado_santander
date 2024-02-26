7ced5d9f4dc1a038fce44d9684bc00b1d7f4af11
aldebarran22 <antonioespinherranz@hotmail.com>
2024-02-26 12:58:47 +0100

    12.58

--- codigo_feb_26_mar_1/objetos.py
+++ codigo_feb_26_mar_1/objetos.py
@@ -23,7 +23,7 @@ class Persona:
 
     def __repr__(self):
         return str(self)
-    
+
     def __lt__(self, other):
         return self.edad < other.edad
 
@@ -40,18 +40,36 @@ class Persona:
         # print("Se elimina a ", self.nombre)
         Persona.contador -= 1
 
-
-if __name__ == "__main__":
+def testPersona():
     print("num personas: ", Persona.getContador())
     p1 = Persona("Ana", 33, 1.8)
     p2 = Persona("Marcos", 36, 1.7)
     # del p2 # fuerza la llamada al destructor __del__
     print("Num personas:", p1.contador, Persona.contador)
     print(p1)
-    #print(str(p1))
-    #print(p1.__str__())
-    L = [p1, p2, Persona("Pedro",44,1.9)]
-    L.sort(key=lambda obj : obj.altura, reverse=True)
+    # print(str(p1))
+    # print(p1.__str__())
+    L = [p1, p2, Persona("Pedro", 44, 1.9)]
+    L.sort(key=lambda obj: obj.altura, reverse=True)
     print(L)
-    L.sort()
+    L.sort()  # Utiliza el operador < __lt__  (less than)
     print(L)
+    #if p1.__lt__(p2):
+    if p1 < p2:
+        print(p1.nombre, "es menor que", p2.nombre)
+    else:
+        print(p2.nombre, "es menor que", p1.nombre)
+
+    # Convertir a json
+    Ljson = [obj.__dict__ for obj in L]
+    print(Ljson)
+    p1.__dict__['tno'] = 914456677
+    print(p1.tno)
+    p1.movil = 656445533
+    print(p1.__dict__)
+
+    
+
+
+if __name__ == "__main__":
+    testPersona()
