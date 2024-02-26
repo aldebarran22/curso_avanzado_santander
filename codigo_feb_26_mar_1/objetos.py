fd56fab2ce1fb7fbe6883789fbdf6f88c9c34b6a
aldebarran22 <antonioespinherranz@hotmail.com>
2024-02-26 12:34:20 +0100

    12.34

--- codigo_feb_26_mar_1/objetos.py
+++ codigo_feb_26_mar_1/objetos.py
@@ -21,6 +21,8 @@ class Persona:
     def __str__(self):
         return self.nombre + " " + str(self.edad) + " " + str(self.altura)
 
+    def __repr__(self):
+        return str(self)
     """
     def metodo(self):
         print("contador:",self.contador)
@@ -31,7 +33,7 @@ class Persona:
         return Persona.contador
 
     def __del__(self):
-        print("Se elimina a ", self.nombre)
+        # print("Se elimina a ", self.nombre)
         Persona.contador -= 1
 
 
@@ -39,8 +41,10 @@ if __name__ == "__main__":
     print("num personas: ", Persona.getContador())
     p1 = Persona("Ana", 33, 1.8)
     p2 = Persona("Marcos", 36, 1.7)
-    del p2
+    # del p2 # fuerza la llamada al destructor __del__
     print("Num personas:", p1.contador, Persona.contador)
     print(p1)
-    print(str(p1))
-    print(p1.__str__())
+    #print(str(p1))
+    #print(p1.__str__())
+    L = [p1, p2, Persona("Pedro",44,1.9)]
+    print(L)
