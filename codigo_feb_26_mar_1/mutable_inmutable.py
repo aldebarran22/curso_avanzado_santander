f0205082effab64833199616d292dca84c56f44e
aldebarran22 <antonioespinherranz@hotmail.com>
2024-02-26 09:55:46 +0100

    9.55

--- codigo_feb_26_mar_1/mutable_inmutable.py
+++ codigo_feb_26_mar_1/mutable_inmutable.py
@@ -27,8 +27,27 @@ def copiaListas():
     L1[0] = 1000
     print('L1',L1, id(L1))
     print('L2',L2, id(L2))
-
+    print('-'*20)
     
+    # Copiar una lista de objetos mutables
+    L1 = [[3,5,6],[3,1,2]]
+    L2 = L1.copy()
+    L1[0] = 1000
+    L1[-1].append(999)
+    print('L1',L1, id(L1))
+    print('L2',L2, id(L2))
+    print('-'*20)
+
+    # Copia profunda de objetos:
+    import copy
+    L1 = [[3,5,6],[3,1,2]]
+    L2 = copy.deepcopy(L1)
+    L1[0] = 1000
+    L1[-1].append(999)
+    print('L1',L1, id(L1))
+    print('L2',L2, id(L2))
+    print('-'*20)
+
 
 if __name__ == '__main__':
     copiaListas()
