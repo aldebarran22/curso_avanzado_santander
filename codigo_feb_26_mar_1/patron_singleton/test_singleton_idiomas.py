9890b9a9b44d4b917740f41723ba4d192d061819
aldebarran22 <antonioespinherranz@hotmail.com>
2024-02-27 10:26:17 +0100

    10.26

--- codigo_feb_26_mar_1/patron_singleton/test_singleton_idiomas.py
+++ codigo_feb_26_mar_1/patron_singleton/test_singleton_idiomas.py
@@ -5,7 +5,7 @@ def prueba():
     print(i18n.getInstance()["twitter"])
     print(i18n.getInstance()["facebook"])
     print(i18n.getInstance("en")["instagram"])
-    print(i18n.getInstance("en")["facebook"])
+    print(i18n.getInstance()["facebook"])
 
 
 prueba()
