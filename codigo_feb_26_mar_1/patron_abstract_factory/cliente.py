51b77b18476527119c35d2c67c64b56f3cc4ad2a
aldebarran22 <antonioespinherranz@hotmail.com>
2024-02-27 10:57:03 +0100

    10.56

--- codigo_feb_26_mar_1/patron_abstract_factory/cliente.py
+++ codigo_feb_26_mar_1/patron_abstract_factory/cliente.py
@@ -8,5 +8,28 @@ from Factoria import Factoria
 from FactoriaApple import FactoriaApple
 from FactoriaSamsung import FactoriaSamsung
 
-print(sys.argv)
-print(Factoria.__subclasses__())
+
+def menu(L):
+    pass
+
+
+if __name__ == "__main__":
+    L = [c.__name__ for c in Factoria.__subclasses__()]
+    if len(sys.argv) >= 2:
+        nombreFact = "Factoria" + sys.argv[1].capitalize()
+        if nombreFact not in L:
+            print("Factoria no soportada")
+            exit()
+        else:
+            nombreFact += "()"
+            factoria = eval(nombreFact)
+
+    else:
+        # lanzar el menu:
+        factoria = menu(L)
+
+    telefono = factoria.createTno()
+    tableta = factoria.createTablet()
+
+    telefono.llamar()
+    tableta.internet()
