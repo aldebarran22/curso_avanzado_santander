501842887b2f0633b35f3558fb68e9fea74b41e1
aldebarran22 <antonioespinherranz@hotmail.com>
2024-02-27 10:04:02 +0100

    10.03

--- codigo_feb_26_mar_1/patron_singleton/singleton_idiomas.py
+++ codigo_feb_26_mar_1/patron_singleton/singleton_idiomas.py
@@ -3,19 +3,26 @@ Implementación del patrón singleton
 Utilidad para i18n
 """
 
+
 def cargaFichero(idioma):
-    path = f"idiomas/{idioma}.txt"
-    print(f'Cargar el fichero: {path}')
-    fich = open(path, "r")
-    palabras = dict()
-    for linea in fich:
-        linea = linea.rstrip()
-        k, _, v = linea.partition("=")
-        palabras[k]=v
-    fich.close()
-    return palabras
+    fich = None
+    try:
+        path = f"idiomas/{idioma}.txt"
+        print(f"Cargar el fichero: {path}")
+        fich = open(path, "r")
+        palabras = dict()
+        for linea in fich:
+            linea = linea.rstrip()
+            k, _, v = linea.partition("=")
+            palabras[k] = v
+        return palabras
+    
+    except  Exception as e:
+        print(e)
+    finally:
+        if fich:fich.close()
+    
 
 if __name__ == "__main__":
-    d = cargaFichero("en")
+    d = cargaFichero("fr")
     print(d)
-
