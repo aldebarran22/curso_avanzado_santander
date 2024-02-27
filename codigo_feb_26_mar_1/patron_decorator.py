7ff7bc35b560899fab9e867aaa47166b2ec24d82
aldebarran22 <antonioespinherranz@hotmail.com>
2024-02-27 14:36:25 +0100

    14.36

--- codigo_feb_26_mar_1/patron_decorator.py
+++ codigo_feb_26_mar_1/patron_decorator.py
@@ -17,3 +17,34 @@ class Ventana(IVentana):
 
     def dibujar(self):
         print(self.titulo,end=" ")
+
+class VentanaDecorator(IVentana):
+
+    def __init__(self, ventana):
+        self.ventana=ventana
+
+class BordeDecorator(VentanaDecorator):
+
+    def __init__(self, ventana):
+        VentanaDecorator.__init__(self,ventana)
+
+    def dibujar(self):
+        pass
+
+class BotonCerrarDecorator(VentanaDecorator):
+
+    def __init__(self, ventana):
+        VentanaDecorator.__init__(self,ventana)
+
+    def dibujar(self):
+        pass
+
+if __name__ == '__main__':
+    v1 = BordeDecorator(Ventana("Ventana con Borde"))
+    v1.dibujar()
+    print()
+
+    v2 = BordeDecorator(BordeDecorator(Ventana("Ventana con Borde")))
+    v2.dibujar()
+    print()
+    
