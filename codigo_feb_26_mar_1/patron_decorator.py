f60b83dc3e950e4c3d68bd3dba16bebdc3b4af86
aldebarran22 <antonioespinherranz@hotmail.com>
2024-02-27 14:39:04 +0100

    14.38

--- codigo_feb_26_mar_1/patron_decorator.py
+++ codigo_feb_26_mar_1/patron_decorator.py
@@ -4,47 +4,55 @@ Patrón decorator
 
 import abc
 
+
 class IVentana(abc.ABC):
 
     @abc.abstractmethod
     def dibujar(self):
         pass
 
+
 class Ventana(IVentana):
 
-    def __init__(self,titulo=""):
-        self.titulo=titulo
+    def __init__(self, titulo=""):
+        self.titulo = titulo
 
     def dibujar(self):
-        print(self.titulo,end=" ")
+        print(self.titulo, end=" ")
+
 
 class VentanaDecorator(IVentana):
 
     def __init__(self, ventana):
-        self.ventana=ventana
+        self.ventana = ventana
+
 
 class BordeDecorator(VentanaDecorator):
 
     def __init__(self, ventana):
-        VentanaDecorator.__init__(self,ventana)
+        VentanaDecorator.__init__(self, ventana)
 
     def dibujar(self):
-        pass
+        print("|", end=" ")
+        self.ventana.dibujar()
+        print("|", end=" ")
+
 
 class BotonCerrarDecorator(VentanaDecorator):
 
     def __init__(self, ventana):
-        VentanaDecorator.__init__(self,ventana)
+        VentanaDecorator.__init__(self, ventana)
 
     def dibujar(self):
-        pass
+        print("[x]", end=" ")
+        self.ventana.dibujar()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     v1 = BordeDecorator(Ventana("Ventana con Borde"))
     v1.dibujar()
     print()
 
-    v2 = BordeDecorator(BordeDecorator(Ventana("Ventana con Borde")))
+    v2 = BordeDecorator(BordeDecorator(Ventana("Ventana con 2 Bordes")))
     v2.dibujar()
     print()
-    
