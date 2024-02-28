1832157583f0648dd4f4dcfee575bd10b354c5d8
aldebarran22 <antonioespinherranz@hotmail.com>
2024-02-28 14:41:47 +0100

    14.41

--- codigo_feb_26_mar_1/pandas_huracan.py
+++ codigo_feb_26_mar_1/pandas_huracan.py
@@ -9,12 +9,18 @@ path = (
     "D:/OneDrive/Escritorio/python_avanzado_santander/practicas/avanzado2/pandas/csv/"
 )
 
+
 def limpiarDF(pathFich):
     df = pd.read_csv(pathFich, sep=";")
     df.columns = [col.rstrip() for col in df.columns]
-    df['Lat'] = pd.to_numeric(df.Lat.str[:-1], downcast="float")
+    df["Lat"] = pd.to_numeric(df.Lat.str[:-1], downcast="float")
+    df["Lon"] = pd.to_numeric(df.Lon.str[:-1], downcast="float")
+    df["Wind"] = pd.to_numeric(df.Wind.str.replace(" mph", ""), downcast="integer")
+    df["Pressure"] = pd.to_numeric(
+        df.Pressure.str.replace(" mb", ""), downcast="integer"
+    )
     df.info()
 
 
-if __name__ == '__main__':
-    limpiarDF(path+"IRMA.csv")
+if __name__ == "__main__":
+    limpiarDF(path + "IRMA.csv")
