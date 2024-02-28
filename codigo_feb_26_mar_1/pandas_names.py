c1c079c1b5c8232a2efd6b5c6ffe13a720b954cb
aldebarran22 <antonioespinherranz@hotmail.com>
2024-02-28 13:35:56 +0100

    13.35

--- codigo_feb_26_mar_1/pandas_names.py
+++ codigo_feb_26_mar_1/pandas_names.py
@@ -30,11 +30,19 @@ def sumarDosAñosOK(año1, año2):
     df2 = cargarAño(año2)
     resul = df1.add(df2, fill_value=0)
     resul.sort_values(by="cuenta", ascending=False, inplace=True)
-    print(resul.head(10))
+    return resul
+
+
+def exportarHTML(df):
+    # Resetear indices:
+    df.reset_index(inplace=True)
+    # print(df.head(10))
+    df.to_html("../ficheros/pedidos.html", index=True)
 
 
 if __name__ == "__main__":
     # df = cargarAño(2007)
     # print(df.head(10))
     # print(df.loc['Madison','F'])
-    sumarDosAñosOK(2007, 2008)
+    df = sumarDosAñosOK(2007, 2008)
+    exportarHTML(df)
