37f2a23f1288ff0d3dca2670fa4c307dd57090be
aldebarran22 <antonioespinherranz@hotmail.com>
2024-02-28 13:19:18 +0100

    13.19

--- codigo_feb_26_mar_1/pandas_names.py
+++ codigo_feb_26_mar_1/pandas_names.py
@@ -18,9 +18,18 @@ def cargarAño(año):
 
 
 def sumarDosAños(año1, año2):
+    # No rellena datos faltantes con 0, muestra NaN
     df1 = cargarAño(año1)
     df2 = cargarAño(año2)
     resul = df1 + df2
+    print(resul.head(10))
+
+
+def sumarDosAñosOK(año1, año2):
+    df1 = cargarAño(año1)
+    df2 = cargarAño(año2)
+    resul = df1.add(df2, fill_value=0)
+    resul.sort_values(by="cuenta", ascending=False, inplace=True)
     print(resul.head(10))
 
 
@@ -28,4 +37,4 @@ if __name__ == "__main__":
     # df = cargarAño(2007)
     # print(df.head(10))
     # print(df.loc['Madison','F'])
-    sumarDosAños(2007, 2008)
+    sumarDosAñosOK(2007, 2008)
