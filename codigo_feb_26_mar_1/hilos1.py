056d7ad5ad24b6e624d970c92d6b182140677969
aldebarran22 <antonioespinherranz@hotmail.com>
2024-02-29 14:35:15 +0100

    14.35

--- codigo_feb_26_mar_1/hilos1.py
+++ codigo_feb_26_mar_1/hilos1.py
@@ -6,6 +6,7 @@ from threading import Thread
 from time import sleep
 from random import randint
 
+
 class Hilo(Thread):
 
     def __init__(self, nombre, n=10):
@@ -14,19 +15,21 @@ class Hilo(Thread):
 
     def run(self):
         for i in range(self.n):
-            print(self.getName()," mensaje: ", (i+1))
-            sleep(randint(0,3))
-        print(self.getName(), 'termina!')
+            print(self.getName(), " mensaje: ", (i + 1))
+            sleep(randint(0, 3))
+        print(self.getName(), "termina!")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     L = []
     nHilos = 4
     for i in range(nHilos):
-        hilo = Hilo(f"Hilo-{i+1}", randint(5,10))
+        hilo = Hilo(f"Hilo-{i+1}", randint(5, 10))
         hilo.start()
         L.append(hilo)
-
-    for hilo in L:
         hilo.join()
 
-    print('Thread-Main termina!')
+    # for hilo in L:
+    #    hilo.join()
+
+    print("Thread-Main termina!")
