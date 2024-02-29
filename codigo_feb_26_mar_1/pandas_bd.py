dd81b82099f372a14c9fd5bbfe112e8d315ad54c
aldebarran22 <antonioespinherranz@hotmail.com>
2024-02-29 09:52:07 +0100

    9.52

--- codigo_feb_26_mar_1/pandas_bd.py
+++ codigo_feb_26_mar_1/pandas_bd.py
@@ -33,8 +33,8 @@ if __name__ == "__main__":
     order by 2 desc"""
     df = bd.getDF(sql)
     df.to_json("../ficheros/categorias.json", indent=4, orient="records")
-    df = bd.getDF("select * from pedidos")
-    df.to_json("../ficheros/pedidos.json", indent=4, orient="records")
+    dfPed = bd.getDF("select * from pedidos")
+    dfPed.to_json("../ficheros/pedidos.json", indent=4, orient="records")
 
     sql = """select c.nombre as categoria from categorias c 
     inner join productos p on c.id = p.idcategoria order by p.id"""
@@ -44,3 +44,9 @@ if __name__ == "__main__":
     df3 = df2.drop_duplicates()
     print("Sin repetidos: ",df3.shape)
 
+    # Iterar por las filas de un DF:
+    i = 3
+    for index, pedido in dfPed.iterrows():
+        print(index, pedido.values)    
+        i-=1
+        if i == 0:break
