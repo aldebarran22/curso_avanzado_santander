a0faa335984e245bf14bc7c4b94e1f84a79a7917
aldebarran22 <antonioespinherranz@hotmail.com>
2024-03-01 13:17:35 +0100

    13.17

--- codigo_feb_26_mar_1/clienteREST1.py
+++ codigo_feb_26_mar_1/clienteREST1.py
@@ -16,7 +16,12 @@ def testListaProductos():
     resp = get(url).json()
     print(resp)
 
+def testReadProducto(id):
+    url = f"http://localhost:5000/productos/{id}"
+    resp = get(url).json()
+    print(resp)
 
 if __name__ == "__main__":
     # testServicioHello()
-    testListaProductos()
+    # testListaProductos()
+    testReadProducto(12)
