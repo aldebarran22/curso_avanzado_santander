5b1c39d8b49034faa06b82a13210f4d6fdd3c76f
aldebarran22 <antonioespinherranz@hotmail.com>
2024-03-01 13:53:07 +0100

    13.53

--- codigo_feb_26_mar_1/clienteREST1.py
+++ codigo_feb_26_mar_1/clienteREST1.py
@@ -2,7 +2,9 @@
 Consumir un servicio de REST con la lib: requests
 """
 
-from requests import get, delete
+from requests import get, delete, post
+from base_datos import Categoria, Producto
+import json
 
 
 def testServicioHello():
@@ -29,8 +31,20 @@ def testDeleteProducto(id):
     print(resp)
 
 
+def testCreateProducto():
+    cat = Categoria(1)
+    prod = Producto(0, "Red Bull", cat, 1.6, 100)
+    dicc = prod.to_json()
+    url = "http://localhost:5000/productos"
+    data = json.dumps(dicc)
+    headers = {"Content-Type": "application/json"}
+    resp = post(url, headers=headers, data=data)
+    print(resp.json())
+
+
 if __name__ == "__main__":
     # testServicioHello()
     # testListaProductos()
     # testReadProducto(12)
-    testDeleteProducto(12)
+    # testDeleteProducto(12)
+    testCreateProducto()
