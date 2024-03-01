5f81b3681be270d6556011386f9c25ff8d94633a
aldebarran22 <antonioespinherranz@hotmail.com>
2024-03-01 14:53:24 +0100

    14.53

--- codigo_feb_26_mar_1/patron_fachada.py
+++ codigo_feb_26_mar_1/patron_fachada.py
@@ -12,10 +12,20 @@ class C:
     # Función puts de C
     __puts = __libc.puts
 
+    # Función strchr de C
+    __strchr = __libc.strchr
+
     @staticmethod
     def puts(cadena):
         C.__puts(cadena.encode("utf8"))
 
+    @staticmethod
+    def strchr(cadena, letra):
+        C.__strchr.restype = c_char_p
+        resul = C.__strchr(cadena.encode("utf8"), ord(letra))
+        return resul.decode('utf8') if resul != None else None
 
 if __name__ == "__main__":
     C.puts("hola que tal")
+    print(C.strchr("hola que tal", "x"))
+    print(C.strchr("hola que tal", "t"))
