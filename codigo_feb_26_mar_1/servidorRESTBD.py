d331058bd22e8778e778b90c6955eb4b0c326ba6
aldebarran22 <antonioespinherranz@hotmail.com>
2024-03-01 13:01:20 +0100

    13.01

--- codigo_feb_26_mar_1/servidorRESTBD.py
+++ codigo_feb_26_mar_1/servidorRESTBD.py
@@ -12,7 +12,7 @@ from base_datos import Empleado, Producto, Categoria
 from base_datos import BaseDatos, path
 
 from flask import Flask
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, abort
 
 # Instanciar la aplicación:
 app = Flask(__name__)
@@ -22,11 +22,16 @@ api = Api(app)
 class RecursoProductos(Resource):
 
     def get(self):
-        pass
+        try:
+            bd = BaseDatos(path)
+            L = bd.select()
+            return [p.to_json() for p in L]
+        except Exception as e:
+            abort(404, message=str(e))
 
 
 # GET: http://localhost:5000/productos
-api.add_resource(RecursoProductos, "/productos")
+api.add_resource(RecursoProductos, "/productos", "/productos/")
 
 if __name__ == "__main__":
     # Poner el servidor en marcha:
