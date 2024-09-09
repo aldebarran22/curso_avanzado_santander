5ec1e34664edcda48f63f842cafbe7c33e7b536b
aldebarran22 <antonioespinherranz@hotmail.com>
2024-09-09 10:38:13 +0200

    10.38

--- codigo_sep_9_13/objetos.py
+++ codigo_sep_9_13/objetos.py
@@ -16,10 +16,24 @@ class Producto:
     def __str__(self):
         return self.nombre+" "+str(self.precio)
     
+    def __lt__(self, other):
+        """
+        if p1 < p2:  -> if p1.__lt__(p2):
+            pass
+        """
+        return self.precio < other.precio
+    
+    
 if __name__ == "__main__":
     p1 = Producto('CocaCola',2.5)    
     print(p1)
 
-    L = [p1, Producto('Fanta Limón',1.7), Producto('Cerveza',2.25)]
+    L = [p1, Producto('Fanta Limón',1.7), Producto('cerveza',2.25)]
     print(L)
+
+    L.sort(key=lambda p : p.nombre.lower())
+    print(L)
+    L.sort()
+    print(L)
+
 
