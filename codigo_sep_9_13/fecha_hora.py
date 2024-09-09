491f834d8e7f8ef6fe38803c5fcf138d08ff0df4
aldebarran22 <antonioespinherranz@hotmail.com>
2024-09-09 12:40:19 +0200

    12.40

--- codigo_sep_9_13/fecha_hora.py
+++ codigo_sep_9_13/fecha_hora.py
@@ -49,6 +49,8 @@ class DateTime(Date,Time):
         Date.__init__(self,dd,mm,yy)
         Time.__init__(self,h,m,s)
 
+    def __str__(self):
+        return Date.__str__(self)+" "+Time.__str__(self)
 
 if __name__ == '__main__':
     t1 = Time(12,3,56)
