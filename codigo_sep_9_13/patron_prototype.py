25b2d98dab70354e94cb5f8ab680460737f140a4
aldebarran22 <antonioespinherranz@hotmail.com>
2024-09-10 11:38:00 +0200

    11.37

---
+++ codigo_sep_9_13/patron_prototype.py
@@ -0,0 +1,77 @@
+"""
+Implementación el patrón prototype 
+Disponemos de una serie de Figuras que se van a clonar mediante
+una factoría.
+
+Dos estrategias posible:
+a) La factoría crea todos los prototipos al principio.
+b) La factoría crea los prototipos bajo demanda.
+"""
+
+import abc
+
+class Prototipo(abc.ABC):
+    def __init__(self, etiqueta="", color="black"):
+        self.etiqueta = etiqueta
+        self.color = color
+
+    def __str__(self):
+        return "etiqueta: " + self.etiqueta + " color: " + self.color
+
+    @abc.abstractmethod
+    def clone(self):
+        pass
+
+
+class Circulo(Prototipo):
+    def __init__(self, etiqueta="circulo", color="black", radio=5.0):
+        Prototipo.__init__(self, etiqueta, color)
+        self.radio = radio
+
+    def __str__(self):
+        return super().__str__() + " radio: " + str(self.radio)
+
+    def clone(self):
+        # El objeto se clona
+        pass
+
+
+class Rectangulo(Prototipo):
+    def __init__(self, etiqueta="rectangulo", color="black", ancho=5.0, alto=10.0):
+        Prototipo.__init__(self, etiqueta, color)
+        self.ancho = ancho
+        self.alto = alto
+
+    def __str__(self):
+        return (
+            super().__str__()
+            + " ancho: "
+            + str(self.ancho)
+            + " alto: "
+            + str(self.alto)
+        )
+
+    def clone(self):
+        # El objeto se clona
+        pass
+
+
+class Triangulo(Prototipo):
+    def __init__(self, etiqueta="triangulo", color="black", base=2.5, altura=8.0):
+        Prototipo.__init__(self, etiqueta, color)
+        self.base = base
+        self.altura = altura
+
+    def __str__(self):
+        return (
+            super().__str__()
+            + " base: "
+            + str(self.base)
+            + " altura: "
+            + str(self.altura)
+        )
+
+    def clone(self):
+        # El objeto se clona
+        pass
+
