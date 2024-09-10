aab5b291a9bb689a639dc3ff70931c1ab9f97d0c
aldebarran22 <antonioespinherranz@hotmail.com>
2024-09-10 13:58:47 +0200

    13.58

--- codigo_sep_9_13/patron_cadena_responsabilidad.py
+++ codigo_sep_9_13/patron_cadena_responsabilidad.py
@@ -26,19 +26,44 @@ class Procesador(abc.ABC):
 class ProcesadorSMS(Procesador):
 
     def procesarPeticion(self, peticion):
-        pass
+        if peticion.destino.lower() == 'sms':
+            print('La petición se enviar por SMS')
+
+        elif self.siguiente != None:
+            print('SMS envía al siguiente la petición')
+            self.siguiente.procesarPeticion(peticion)
+
+        else:
+            print('SMS fin de cadena, no se puede procesar la petición')
 
 
 class ProcesadorEmail(Procesador):
 
     def procesarPeticion(self, peticion):
-        pass
+        if peticion.destino.lower() == 'email':
+            print('La petición se enviar por email')
+
+        elif self.siguiente != None:
+            print('EMAIL envía al siguiente la petición')
+            self.siguiente.procesarPeticion(peticion)
+
+        else:
+            print('EMAIL fin de cadena, no se puede procesar la petición')
 
 
 class ProcesadorWhatsApp(Procesador):
 
     def procesarPeticion(self, peticion):
-        pass
+        if peticion.destino.lower() == 'whatsapp':
+            print('La petición se enviar por WhatsApp')
+
+        elif self.siguiente != None:
+            print('WhatsApp envía al siguiente la petición')
+            self.siguiente.procesarPeticion(peticion)
+
+        else:
+            print('WhatsApp fin de cadena, no se puede procesar la petición')
+
 
 
 if __name__=='__main__':
