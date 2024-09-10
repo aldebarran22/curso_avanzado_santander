341c6c670e089242ca7892eab62334359564823a
aldebarran22 <antonioespinherranz@hotmail.com>
2024-09-10 09:27:48 +0200

    9.27

--- codigo_sep_9_13/patron_singleton.py
+++ codigo_sep_9_13/patron_singleton.py
@@ -2,24 +2,6 @@
 Patrón de creación : Singleton
 """
 
-def cargaIdioma(idioma):
-    path = f"idiomas/{idioma}.txt"
-    fich = None
-    try:
-        d = dict()
-        fich = open(path, 'r')
-        for linea in fich:
-            linea = linea.rstrip()
-            k, _, v = linea.partition('=')
-            d[k] = v
-
-        return d
-
-    except Exception as e:
-        print(e.__class__.__name__,e)       
-    
-    finally:
-        if fich: fich.close()
 
 class Singleton:
 
@@ -29,15 +11,36 @@ class Singleton:
     @staticmethod
     def getInstance(idioma="es"):
         
-        if Singleton.__palabras is None:
+        if Singleton.__palabras is None or Singleton.__idioma != idioma:
             # Cargar fichero
             print(f'Cargamos el fichero de {idioma}')
+            Singleton.__palabras = Singleton.__cargaIdioma(idioma)
+            Singleton.__idioma = idioma
             
         return Singleton.__palabras
+    
+    @staticmethod
+    def __cargaIdioma(idioma):
+        path = f"idiomas/{idioma}.txt"
+        fich = None
+        try:
+            d = dict()
+            fich = open(path, 'r')
+            for linea in fich:
+                linea = linea.rstrip()
+                k, _, v = linea.partition('=')
+                d[k] = v
 
-if __name__ == '__main__':    
-    d = cargaIdioma("es")
-    print(d)
+            return d
 
-    Singleton.getInstance()["inicio"]
+        except Exception as e:
+            print(e.__class__.__name__,e)       
+        
+        finally:
+            if fich: fich.close()
+
+if __name__ == '__main__':        
+    print(Singleton.getInstance()["inicio"])
+    print(Singleton.getInstance()["volver"])
+    print(Singleton.getInstance()["nombre"])
 
