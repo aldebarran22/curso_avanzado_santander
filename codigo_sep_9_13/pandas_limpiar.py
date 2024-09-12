22544afbbe031684e4af2020acc06157f93cc5ed
aldebarran22 <antonioespinherranz@hotmail.com>
2024-09-12 09:31:20 +0200

    9.31

--- codigo_sep_9_13/pandas_limpiar.py
+++ codigo_sep_9_13/pandas_limpiar.py
@@ -11,6 +11,16 @@ def cargaFichero(path):
     # Quitar espacios de los nombres de las columnas
     df.columns = [col.strip() for col in df.columns]
     df["Lat"] = pd.to_numeric(df.Lat.str[:-1], downcast="float")
+    df["Lon"] = pd.to_numeric(df.Lon.str[:-1], downcast="float")
+    df["Wind"] = pd.to_numeric(df.Wind.str.replace(" mph",""), downcast="integer")
+    df["Pressure"] = pd.to_numeric(df.Pressure.str.replace(" mb",""), downcast="integer")
+    # 2005 sep-16 15:00 GMT
+    df["Datetime"] = pd.to_datetime(
+        "2005 " + df.Date + " " + df.Time.str.replace(" GMT",""),
+        format="%Y %b %d %H:%M"
+    )
+    df.drop(columns=["Date","Time"], inplace=True)
+    df = df[["Datetime","Lat","Lon","Wind","Pressure"]]
     return df
 
 
@@ -18,5 +28,4 @@ if __name__ == '__main__':
     path = "../ficheros_curso/csv/MARIA.csv"
     df = cargaFichero(path)
     df.info()
-    print(df.head())
-    print(df.Lat)
+    print(df.head())    
