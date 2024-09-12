41dfec8de0b146fa91e33b8ebf438a8a59bab0e9
aldebarran22 <antonioespinherranz@hotmail.com>
2024-09-12 11:55:48 +0200

    11.55

--- codigo_sep_9_13/formatos/formato_xml.py
+++ codigo_sep_9_13/formatos/formato_xml.py
@@ -8,7 +8,22 @@ from xml.etree.ElementTree import Element, SubElement, Comment, tostring
 from base_datos import path, BaseDatos, Categoria, Producto
 
 def exportarXML(pathFile):
-    pass
+    try:
+        bd = BaseDatos(path)
+        categorias = bd.selectCategorias()
+        raiz = Element("categorias")
+        raiz.set("version","1.0")
+        comentario = Comment("Categorias y productos de la BD")
+        raiz.append(comentario)
+        for cat in categorias:
+            categoria = SubElement(raiz, "categoria")
+            nombreCategoria = SubElement(categoria, "nombre")
+            nombreCategoria.text = cat.nombre
+
+        print(tostring(raiz))
+
+    except Exception as e:
+        print(e)
 
 if __name__ == '__main__':
     exportarXML("productos.xml")
