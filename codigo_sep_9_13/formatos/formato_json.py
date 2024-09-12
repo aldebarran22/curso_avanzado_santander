371fd5b577295ece978b6d2529534f750329af16
aldebarran22 <antonioespinherranz@hotmail.com>
2024-09-12 10:41:30 +0200

    10.41

--- codigo_sep_9_13/formatos/formato_json.py
+++ codigo_sep_9_13/formatos/formato_json.py
@@ -6,14 +6,21 @@ import json
 from base_datos import path, BaseDatos, Categoria, Producto
 
 
-def exportarJson():    
+def exportarJson():   
+    fich = None 
     try:
         bd = BaseDatos(path)
-        categoria = bd.select()
-        print(categoria)
+        fich = open("productos.json","w")
+        categorias = bd.select()
+        cat_json = [cat.to_json() for cat in categorias]
+        #print(cat_json[:3])
+        json.dump(cat_json, fich, indent=4)
 
     except Exception as e:
         print(e.__class__.__name__, e)
+
+    finally:
+        if fich: fich.close()
 
 
 def importarJson():
