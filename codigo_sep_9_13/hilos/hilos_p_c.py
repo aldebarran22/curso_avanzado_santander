5e0d43e7a5503d6e25e4cae2591112b2b9bc8350
aldebarran22 <antonioespinherranz@hotmail.com>
2024-09-13 13:44:13 +0200

    13.44

--- codigo_sep_9_13/hilos/hilos_p_c.py
+++ codigo_sep_9_13/hilos/hilos_p_c.py
@@ -7,11 +7,11 @@ from threading import Thread, Lock, Semaphore
 from random import randint
 from time import sleep
 
-num_muestras = 10
+num_muestras = 12
 tam_buffer = 5
 
 num_productores = 2
-num_consumidores = 1
+num_consumidores = 3
 
 
 class Productor(Thread):
