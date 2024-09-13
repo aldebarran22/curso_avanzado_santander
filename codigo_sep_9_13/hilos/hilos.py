c49e7472d320a6da00952845fcabb572ce3511ca
aldebarran22 <antonioespinherranz@hotmail.com>
2024-09-13 12:10:36 +0200

    12.10

--- codigo_sep_9_13/hilos/hilos.py
+++ codigo_sep_9_13/hilos/hilos.py
@@ -4,34 +4,40 @@ Threads:
 
 from threading import Thread
 from random import randint
+from time import sleep
+
 
 class ThreadMensajes(Thread):
 
-    def __init__(self, nombre="", n = 10):
+    def __init__(self, nombre="", n=10):
         Thread.__init__(self, name=nombre)
         self.n = n
 
     def run(self):
         for i in range(self.n):
-            print(f"{self.name} - ", f"mensaje {i+1}" )
-        print(self.name,"termina")
+            print(f"{self.name} - ", f"mensaje {i+1}")
+            sleep(randint(1, 3))
+        print(self.name, "termina")
+
 
 class ThreadRandom(Thread):
 
-    def __init__(self, nombre="", n = 10):
+    def __init__(self, nombre="", n=10):
         Thread.__init__(self, name=nombre)
         self.n = n
 
     def run(self):
         for i in range(self.n):
-            print(f"{self.name} - {i+1} -> ", randint(1,10))
-        print(self.name,"termina")
+            print(f"{self.name} - {i+1} -> ", randint(1, 10))
+            sleep(randint(2, 4))
+        print(self.name, "termina")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     m = ThreadMensajes("mensajes", 15)
     r = ThreadRandom("aleatorio", 13)
 
     m.start()
     r.start()
 
-    print('main termina ...')
+    print("main termina ...")
