e93fa2e2411258b2868937ba61f0096d54084534
aldebarran22 <antonioespinherranz@hotmail.com>
2024-09-13 09:59:01 +0200

    9.58

--- codigo_sep_9_13/servicios/servidor_rest.py
+++ codigo_sep_9_13/servicios/servidor_rest.py
@@ -3,15 +3,19 @@ Servidor REST
 """
 
 from flask import Flask
-from flask_restful import Resource , Api
+from flask_restful import Resource, Api
+
 app = Flask(__name__)
 api = Api(app)
 
+
 class HelloWorld(Resource):
+
     def get(self):
         return {"hello": "world"}
-    
-api.add_resource(HelloWorld ,"/")
 
-if __name__=="__main__":
-    app.run(debug =True) 
+
+api.add_resource(HelloWorld, "/")
+
+if __name__ == "__main__":
+    app.run(debug=True)
