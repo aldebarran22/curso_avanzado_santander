bee383a7f75e49919e1392cce114e2cb0919debe
aldebarran22 <antonioespinherranz@hotmail.com>
2024-09-16 14:55:42 +0200

    14.55

--- codigo_sep_16_20/patron_singleton.py
+++ codigo_sep_16_20/patron_singleton.py
@@ -4,38 +4,50 @@ Patrón singleton
 
 from os.path import isfile
 
-def cargaIdioma(idioma):
-    path = f"idiomas/{idioma}.txt"
-    fich = None
-    d = dict()
-    try:
-        if not isfile(path):
-            raise FileNotFoundError(f"No existe el fichero: {path}")
 
-        fich = open(path, "r")
-        for fila in fich:
-            fila = fila.rstrip()
-            k, _, v = fila.partition("=")
-            d[k]=v
+class SingletonIdioma:
 
-        return d
+    __palabras = None
+    __idioma = None
 
-    except Exception as e:        
-        raise e
+    @staticmethod
+    def getInstance(idioma="es"):
 
-    finally:
-        if fich: fich.close()
+        if SingletonIdioma.__palabras is None or SingletonIdioma.__idioma != idioma:
+            print("Cargando fichero de: ", idioma)
+            SingletonIdioma.__palabras = SingletonIdioma.__cargaIdioma(idioma)
+            SingletonIdioma.__idioma = idioma
+
+        return SingletonIdioma.__palabras
+
+
+    @staticmethod
+    def __cargaIdioma(idioma):
+        path = f"idiomas/{idioma}.txt"
+        fich = None
+        d = dict()
+        try:
+            if not isfile(path):
+                raise FileNotFoundError(f"No existe el fichero: {path}")
+
+            fich = open(path, "r")
+            for fila in fich:
+                fila = fila.rstrip()
+                k, _, v = fila.partition("=")
+                d[k]=v
+
+            return d
+
+        except Exception as e:        
+            raise e
+
+        finally:
+            if fich: fich.close()
+
 
 
 if __name__=='__main__':
-    try:
-        d = cargaIdioma("en")
-        print(d)
-    except Exception as e:
-        print(e)
+    print(SingletonIdioma.getInstance()["inicio"]) # Carga en la primera llamada.
+    print(SingletonIdioma.getInstance()["volver"]) # Utilizan el anterior no carga de nuevo.
+    print(SingletonIdioma.getInstance()["nombre"])
 
-"""
-print(SingletonIdioma.getInstance()["inicio"]) # Carga en la primera llamada.
-print(SingletonIdioma.getInstance()["volver"]) # Utilizan el anterior no carga de nuevo.
-print(SingletonIdioma.getInstance()["nombre"])
-"""
