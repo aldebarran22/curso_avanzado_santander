cf8d6caf6e9fbc2f4de3cbeb4059f5b86c512dea
aldebarran22 <antonioespinherranz@hotmail.com>
2024-09-16 14:28:03 +0200

    14.27

--- codigo_sep_16_20/singleton/ejemplo.py
+++ codigo_sep_16_20/singleton/ejemplo.py
@@ -14,20 +14,23 @@ def singleton(refresh=False):
     print("Singleton creado")    
     return singleton.instancia
 
-# Asocia un atributo a una funcion
-singleton.instancia=""
+
+if __name__=='__main__':
+    print(type(singleton))
+    # Asocia un atributo a una funcion
+    singleton.instancia=""
 
 
-print("Sin refresco, siempre la misma hora")
-print(singleton())
-time.sleep(1)
-print("siguiente llamada")
-print(singleton())
-print()
-print("Con refresco, cada llamada cambia la hora")
-print(singleton(True))
-time.sleep(1)
-print(singleton(True))
+    print("Sin refresco, siempre la misma hora")
+    print(singleton())
+    time.sleep(1)
+    print("siguiente llamada")
+    print(singleton())
+    print()
+    print("Con refresco, cada llamada cambia la hora")
+    print(singleton(True))
+    time.sleep(1)
+    print(singleton(True))
 
 
 
