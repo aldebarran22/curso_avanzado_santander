76cad508ba7976aad8a397cdc675421cb3d8ccb8
aldebarran22 <antonioespinherranz@hotmail.com>
2024-09-16 14:42:24 +0200

    14.42

--- codigo_sep_16_20/patron_singleton.py
+++ codigo_sep_16_20/patron_singleton.py
@@ -7,6 +7,7 @@ from os.path import isfile
 def cargaIdioma(idioma):
     path = f"idiomas/{idioma}.txt"
     fich = None
+    d = dict()
     try:
         if not isfile(path):
             raise FileNotFoundError(f"No existe el fichero: {path}")
@@ -14,14 +15,21 @@ def cargaIdioma(idioma):
         fich = open(path, "r")
         for fila in fich:
             fila = fila.rstrip()
-            print(fila)
+            k, _, v = fila.partition("=")
+            d[k]=v
+            
+        return d
 
-    except Exception as e:
-        print(e)
+    except Exception as e:        
         raise e
 
     finally:
         if fich: fich.close()
 
 
-cargaIdioma("es")
+if __name__=='__main__':
+    try:
+        d = cargaIdioma("en")
+        print(d)
+    except Exception as e:
+        print(e)
