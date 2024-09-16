fca130aece2d453ea0a9e9eddf82bfef7ffae772
aldebarran22 <antonioespinherranz@hotmail.com>
2024-09-16 13:33:30 +0200

    13.33

---
+++ codigo_sep_16_20/polimorfimo_parametrico.py
@@ -0,0 +1,31 @@
+from functools import singledispatch
+
+@singledispatch
+def func(arg):
+	print('Comportamiento por defecto')
+	
+@func.register(int)
+@func.register(float)
+def _(arg):
+	print('Recibo el numero ',arg)
+	
+class Clase:
+	pass
+	
+@func.register(Clase)
+def _(arg):
+	print('un objeto de Clase')
+	
+@func.register(list)
+@func.register(set)
+def _(arg):
+	print('Elementos de la colección: ', len(arg))
+	
+if __name__ == '__main__':
+	func('hola')
+	func(888)
+	func(8.99)
+	a = Clase()
+	func(a)
+	func([1,2,3,4,5])
+	func({1,2,3,4,5})
