3e08b3ef9424c2fcc50ea33948da989220849de9
aldebarran22 <antonioespinherranz@hotmail.com>
2024-09-17 14:05:10 +0200

    14.05

--- codigo_sep_16_20/patron_builder/builder.py
+++ codigo_sep_16_20/patron_builder/builder.py
@@ -5,6 +5,7 @@ Conversor de formato CSV a HTML o XML
 
 import abc
 
+
 class Builder(abc.ABC):
 
     @abc.abstractmethod
@@ -19,36 +20,38 @@ class Builder(abc.ABC):
     def crearFichero(self, texto, path):
         pass
 
+
 class BuilderXML(Builder):
-    
+
     def createCab(self, L):
         pass
 
-   
     def createDetalle(self, L):
         pass
 
     def crearFichero(self, texto, path):
+        pass
+
 
 class BuilderHTML(Builder):
-    
+
     def createCab(self, L):
         pass
 
-   
     def createDetalle(self, L):
         pass
 
     def crearFichero(self, texto, path):
         pass
+
 
 class Director:
 
-    def __init__(self,builder):
+    def __init__(self, builder):
         self.builder = builder
 
     def convertirFichero(self, path, sep=";"):
-        f=None
+        f = None
         nombre = path.partition(".")[0]
         try:
             f = open(path)
@@ -57,11 +60,14 @@ class Director:
                 print(linea)
         except Exception as e:
             print(e)
-        finally:
-            if f: f.close()
 
-if __name__ == '__main__':
-    
+        finally:
+            if f:
+                f.close()
+
+
+if __name__ == "__main__":
+
     builder = BuilderXML()
     director = Director(builder)
     director.convertirFichero("Empleados.txt")
