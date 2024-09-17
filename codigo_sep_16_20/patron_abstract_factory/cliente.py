fdcf4c082937edbb1c75119188a55603e6552c26
aldebarran22 <antonioespinherranz@hotmail.com>
2024-09-17 09:31:52 +0200

    9.31

--- codigo_sep_16_20/patron_abstract_factory/cliente.py
+++ codigo_sep_16_20/patron_abstract_factory/cliente.py
@@ -4,8 +4,15 @@ from FactoriaApplet import FactoriaApplet
 from FactoriaSamsung import FactoriaSamsung
 
 def seleccionarFactoria():
-    L = [c.__name__ for c in Factoria.__subclasses__()]
-    print(L)
+    L = [c for c in Factoria.__subclasses__()]
+    print("MENU")
+    for pos, clase in enumerate(L):
+        print(pos+1, clase.__name__)
+    op = int(input("Opción: "))
+    return L[op-1]
+
+    
+
 
 
 if __name__ == '__main__':
