29ee8af298b4fb3bc21386bdad92d5f7e80bd173
aldebarran22 <antonioespinherranz@hotmail.com>
2024-09-18 12:03:07 +0200

    12.03

---
+++ codigo_sep_16_20/pandas_pedidos.py
@@ -0,0 +1,12 @@
+"""
+Con la libreria de pandas generar un fichero
+por cada pedido.
+"""
+
+import pandas as pd
+
+def exportarPaises():
+    pass
+
+if __name__ == '__main__':
+    exportarPaises()
