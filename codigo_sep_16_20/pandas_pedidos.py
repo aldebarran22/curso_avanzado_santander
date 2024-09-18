9fb13607729bbdeab64ca4a2805e7cbe40633298
aldebarran22 <antonioespinherranz@hotmail.com>
2024-09-18 12:19:06 +0200

    12.19

--- codigo_sep_16_20/pandas_pedidos.py
+++ codigo_sep_16_20/pandas_pedidos.py
@@ -5,8 +5,12 @@ por cada pedido.
 
 import pandas as pd
 
-def exportarPaises():
-    pass
+def exportarPaises(path):
+    dt = pd.read_csv(path, sep=";")
+    paises = sorted(dt.pais.unique())
+    for p in paises:
+        path_pais = f"../ficheros/pedidos/{p}.csv"
+        dt[dt.pais==p].to_csv(path_pais, index=False, decimal=",", sep=";")
 
 if __name__ == '__main__':
-    exportarPaises()
+    exportarPaises('../ficheros_curso/merge/Pedidos.txt')
