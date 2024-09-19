b39518a9ac10c6601e93cf4bf44aa8b7f054da23
aldebarran22 <antonioespinherranz@hotmail.com>
2024-09-19 13:01:48 +0200

    13.01

--- codigo_sep_16_20/formatos/formatoJSON.py
+++ codigo_sep_16_20/formatos/formatoJSON.py
@@ -1,17 +1,28 @@
 """
 Generar ficheros de json con la librería json
 """
+
 import json
 from base_datos import Categoria, Producto, path, BaseDatos
 
+
 def extraerProductos():
+    fich = None
     try:
         bd = BaseDatos(path)
+        fich = open("productos.json", "w")
         productos = bd.select()
-        print(productos[:3])
+        L = [p.to_json() for p in productos]
+        print("Fichero json generado")
+        json.dump(L, fich, indent=4)
 
-    except  Exception as e:
+    except Exception as e:
         print(e)
 
-if __name__ == '__main__':
+    finally:
+        if fich:
+            fich.close()
+
+
+if __name__ == "__main__":
     extraerProductos()
