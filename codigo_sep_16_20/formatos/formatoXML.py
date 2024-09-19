241401e80f4d18605fd8e0f16d925b4ba202ca55
aldebarran22 <antonioespinherranz@hotmail.com>
2024-09-19 14:24:34 +0200

    14.24

--- codigo_sep_16_20/formatos/formatoXML.py
+++ codigo_sep_16_20/formatos/formatoXML.py
@@ -15,11 +15,14 @@ def exportarXML():
             categoria = SubElement(raiz, "categoria",{"idcat":str(cat.id)})
             nombreCat = SubElement(categoria, "nombre")
             nombreCat.text = cat.nombre
-            
+
             listaProd = bd.select(cat.nombre)
+            productos = SubElement(categoria, "productos")
 
             for prod in listaProd:
-                pass
+                producto = SubElement(productos, "producto", {"idprod":str(prod.id)})
+        
+        
         print(tostring(raiz))
             
     except Exception as e:
