8676184569bc7df5f983e22d18e0d3a75460ec19
aldebarran22 <antonioespinherranz@hotmail.com>
2024-09-20 10:52:14 +0200

    10.52

--- codigo_sep_16_20/servicios/cliente_rest.py
+++ codigo_sep_16_20/servicios/cliente_rest.py
@@ -29,6 +29,10 @@ def testProductos():
         if fich:
             fich.close()
 
+def testDelete():
+    url = "http://localhost:5000/productos/3"
+    resp = requests.get(url)
+    print(resp.json())
 
 if __name__ == "__main__":
-    testProductos()
+    testDelete()
