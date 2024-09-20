ce25c072f18be28c81ef9169cb91a88cf29ac2b9
aldebarran22 <antonioespinherranz@hotmail.com>
2024-09-20 11:59:00 +0200

    11.58

---
+++ codigo_sep_16_20/hilos/hilos.py
@@ -0,0 +1,33 @@
+"""
+Implementación de hilos con la clase Thread y
+problemas al esperar un hilo: join
+"""
+
+from threading import Thread
+from random import randint
+from time import sleep
+
+
+class ThreadRandom(Thread):
+
+    def __init__(self, nombre, n):
+        Thread.__init__(self, name=nombre)
+        self.L = []
+        self.n = n
+
+    def run(self):
+        for i in range(self.n):
+            num = randint(1, 10)
+            self.L.append(num)
+            sleep(randint(1, 3))
+        print("Termina: ", self.name)
+
+
+if __name__ == "__main__":
+    hilos = []
+    n_hilos = 3
+    for i in range(n_hilos):
+        nombre = f"Hilo-{i+1}"
+        hilo = ThreadRandom(nombre, randint(5, 15))
+        hilo.start()
+    print("Termina main")
