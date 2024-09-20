348ec89966f1d2730da956d57286610a95054e79
aldebarran22 <antonioespinherranz@hotmail.com>
2024-09-20 14:08:57 +0200

    14.08

--- codigo_sep_16_20/mensajes_log.py
+++ codigo_sep_16_20/mensajes_log.py
@@ -5,7 +5,15 @@ Pruebas con la librería logging
 import logging
 
 def mensajesConsola():
-    logging.basicConfig(level=logging.DEBUG)
+    logging.basicConfig(level=logging.ERROR)
+    logging.debug("mensaje debug")
+    logging.info("mensaje info")
+    logging.warning("mensaje warning")
+    logging.error("mensaje error")
+    logging.critical("mensaje critical")
+
+def mensajesFichero():
+    logging.basicConfig(filename="../ficheros/ejemplo.log", filemode='w',level=logging.ERROR)
     logging.debug("mensaje debug")
     logging.info("mensaje info")
     logging.warning("mensaje warning")
@@ -14,4 +22,5 @@ def mensajesConsola():
 
     
 if __name__ == "__main__":
-    mensajesConsola()
+    #mensajesConsola()
+    mensajesFichero()
