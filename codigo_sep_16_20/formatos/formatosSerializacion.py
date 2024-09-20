42cb9c3fdee81e9ee9a981281462f30f3a50e8a0
aldebarran22 <antonioespinherranz@hotmail.com>
2024-09-20 09:15:38 +0200

    9.15

---
+++ codigo_sep_16_20/formatos/formatosSerializacion.py
@@ -0,0 +1,26 @@
+"""
+Serialización de objetos con las librerías:
+pickle y shelve.
+"""
+
+import pickle
+import shelve
+
+from base_datos import BaseDatos, path, Producto, Categoria
+
+def serializarPickle(pathFile, objeto):
+    fich = None
+    try:
+        fich = open(pathFile, "wb")
+        pickle.dump(objeto, fich)
+    except Exception as e:
+        print(e)
+    finally:
+        if fich: fich.close()
+
+
+if __name__ == '__main__':
+    bd = BaseDatos(path)
+    prod = bd.read(1)
+    print(prod)
+    serializarPickle("pickle_producto.bin", prod)
