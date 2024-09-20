0375c37bf0f4fdb8878fa806a6bcfcac29f0ac07
aldebarran22 <antonioespinherranz@hotmail.com>
2024-09-20 10:07:55 +0200

    10.07

---
+++ codigo_sep_16_20/servicios/servidor_rest.py
@@ -0,0 +1,16 @@
+"""
+Hello World en un servicio de Rest
+"""
+
+from flask import Flask
+from flask_restful import Resource , Api
+app = Flask(__name__)
+api = Api(app)
+
+class HelloWorld(Resource):
+    def  get(self):
+        return {"hello": "world"}
+    
+api.add_resource(HelloWorld ,"/")
+if __name__=="__main__":
+    app.run(debug =True) # Petición : http://localhost:5000
