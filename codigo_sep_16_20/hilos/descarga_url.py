4c50055b61e666b96bfa5116e3494cbb982f07e0
aldebarran22 <antonioespinherranz@hotmail.com>
2024-09-20 13:29:26 +0200

    13.29

--- codigo_sep_16_20/hilos/descarga_url.py
+++ codigo_sep_16_20/hilos/descarga_url.py
@@ -3,25 +3,54 @@ Codigo de ejemplo para descargar una URL
 """
 
 import urllib.request as urllib2
+from threading import Thread
 
 
-def descargaURL(url):
-	f = None
-	numero = None
-	
-	try:
-		f = urllib2.urlopen(url)                
-		numero = int(f.read())
-		print (numero)
+class Download(Thread):
+
+	def __init__(self, ini, fin):
+		Thread.__init__(self)
+		self.ini = ini
+		self.fin = fin
+
+	def run(self):
+		for i in range(self.ini, self.fin):
+			url = f"http://localhost:8000/fich{i}.txt"
+			print(url)
+			numero = self.__descargaURL(url)
+			print(numero)
+
+	def __descargaURL(self, url):
+		f = None
+		numero = None
 		
-	except Exception as e:
-		print("ERROR: ", e)
-		
-	finally:
-		if f != None: f.close()
-		return numero
+		try:
+			f = urllib2.urlopen(url)                
+			numero = int(f.read())
+			return numero
+				
+		except Exception as e:
+			print("ERROR: ", e)
+			
+		finally:
+			if f != None: f.close()
+			
 
 if __name__=='__main__':
-	url = 'http://localhost:8000/fich10.txt'
-	num = descargaURL(url)
-	print('Numero : ', num)
+	num_hilos = 5
+	num_ficheros = 100
+	num_fich_hilo = num_ficheros // num_hilos
+
+	hilos = []
+	L = [(i*num_fich_hilo, i*num_fich_hilo+num_fich_hilo) for i in range(num_hilos)]
+	
+	for pos, i in enumerate(range(num_hilos)):
+		d = Download(*L[i])
+		d.start()
+		hilos.append(d)
+
+	for h in hilos:
+		h.join()
+
+
+	
