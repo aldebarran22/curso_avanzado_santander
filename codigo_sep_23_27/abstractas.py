98870d58636a1080f05675cd9381036a7971fbf1
aldebarran22 <antonioespinherranz@hotmail.com>
2024-09-23 12:22:02 +0200

    12.21

--- codigo_sep_23_27/abstractas.py
+++ codigo_sep_23_27/abstractas.py
@@ -31,9 +31,43 @@ class Circulo(Figura):
 
     def area(self):
         return math.pi * self.radio**2
+    
+class Rectangulo(Figura):
+    def __init__(self, etiqueta="rectangulo", color=0, ancho=5.0, alto=10.0):
+        Figura.__init__(self, etiqueta, color)
+        self.ancho = ancho
+        self.alto = alto
+
+    def __str__(self):
+        return (
+            super().__str__()
+            + " ancho: "
+            + str(self.ancho)
+            + " alto: "
+            + str(self.alto)
+        )
+    
+class Triangulo(Figura):
+    def __init__(self, etiqueta="triangulo", color=0, base=2.5, altura=8.0):
+        Figura.__init__(self, etiqueta, color)
+        self.base = base
+        self.altura = altura
+
+    def __str__(self):
+        return (
+            super().__str__()
+            + " base: "
+            + str(self.base)
+            + " altura: "
+            + str(self.altura)
+        )
 
 if __name__ == "__main__":
     try:
         fig = Figura("circulo", 5)
     except Exception as e:
         print(e.__class__.__name__, e)
+
+    cir1 = Circulo("circulo1", 12, 5.8)
+    print(cir1.area())
+
