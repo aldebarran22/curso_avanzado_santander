e5838e18ffd9a8b8f8a24c8eda2a7b675e218ebf
aldebarran22 <antonioespinherranz@hotmail.com>
2024-09-23 09:50:15 +0200

    9.50

---
+++ codigo_sep_23_27/conversor_csv_json.py
@@ -0,0 +1,21 @@
+"""Convertir texto en CSV a json"""
+
+csv="""id;nombre;cargo
+1;Davolio;Representante de ventas
+2;Fuller;Vicepresidente comercial
+3;Leverling;Representante de ventas
+4;Peacock;Representante de ventas
+5;Buchanan;Gerente de ventas
+6;Suyama;Representante de ventas
+7;King;Representante de ventas
+8;Callahan;Coordinador ventas interno
+9;Dodsworth;Representante de ventas"""
+
+def tojson(formato_csv):
+    pass
+
+def tocsv(formato_json):
+    pass
+
+if __name__ == '__main__':
+    pass
