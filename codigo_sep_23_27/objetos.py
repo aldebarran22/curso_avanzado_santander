7d7af378b2cf30876570b08398797c15ba2cb517
aldebarran22 <antonioespinherranz@hotmail.com>
2024-09-23 10:33:54 +0200

    10.33

--- codigo_sep_23_27/objetos.py
+++ codigo_sep_23_27/objetos.py
@@ -11,8 +11,15 @@ class Producto:
         self.precio = precio
         self.exis = exis
 
+    def __str__(self):
+        return " ".join(k + "=" + str(v) for k, v in self.__dict__.items())
+
+
 def testProducto():
-    pass
+    p1 = Producto(10, "CocaCola",1.5, 100)
+    p1.__dict__['iva']=round(p1.precio * 0.21,2)
+    p1.descuento = 0.05
+    print(p1) # p1.__str__()
 
 if __name__ == '__main__':
     testProducto()
