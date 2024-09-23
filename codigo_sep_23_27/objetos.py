900700153ab7749731368a8adfa899205da17348
aldebarran22 <antonioespinherranz@hotmail.com>
2024-09-23 10:24:37 +0200

    10.24

---
+++ codigo_sep_23_27/objetos.py
@@ -0,0 +1,18 @@
+"""
+POO en python
+"""
+
+class Producto:
+    """Implementación de la clase producto"""
+
+    def __init__(self, id=0, nombre="", precio=0.0, exis=0):
+        self.id = id
+        self.nombre = nombre
+        self.precio = precio
+        self.exis = exis
+
+def testProducto():
+    pass
+
+if __name__ == '__main__':
+    testProducto()
