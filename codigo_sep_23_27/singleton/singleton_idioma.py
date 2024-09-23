360b8ff322bc23394590351fcd194726771cad60
aldebarran22 <antonioespinherranz@hotmail.com>
2024-09-23 14:12:04 +0200

    14.12

--- codigo_sep_23_27/singleton/singleton_idioma.py
+++ codigo_sep_23_27/singleton/singleton_idioma.py
@@ -3,25 +3,45 @@ Patrón singleton para la carga de un fichero
 de idioma
 """
 
+class SingletonIdioma:
 
-def cargarFichero(idioma):
-    path = f"idiomas/{idioma}.txt"
-    fich = None
-    dicc = dict()
-    try:
-        fich = open(path, "r")
-        for linea in fich:
-            linea = linea.rstrip()
-            k, _, v = linea.partition('=')
-            dicc[k] = v
-        return dicc
+    # Variable de clase:
+    __palabras = None
+    __idioma = None
 
-    except Exception as e:
-        print(e)
-    finally:
-        if fich:
-            fich.close()
+    @staticmethod
+    def getInstance(idioma='es'):
+
+        if SingletonIdioma.__palabras is None or SingletonIdioma.__idioma != idioma:
+            # Es la primera vez o hay un cambio de idioma:
+            print("Carga del fichero: "+idioma)
+            SingletonIdioma.__palabras = SingletonIdioma.__cargarFichero(idioma)
+            SingletonIdioma.__idioma = idioma
+        
+        return SingletonIdioma.__palabras
+
+    @staticmethod
+    def __cargarFichero(idioma):
+        path = f"idiomas/{idioma}.txt"
+        fich = None
+        dicc = dict()
+        try:
+            fich = open(path, "r")
+            for linea in fich:
+                linea = linea.rstrip()
+                k, _, v = linea.partition('=')
+                dicc[k] = v
+            return dicc
+
+        except Exception as e:
+            raise e
+        
+        finally:
+            if fich:
+                fich.close()
 
 if __name__=='__main__':
-    dicc = cargarFichero("es")            
-    print(dicc)
+   print(SingletonIdioma.getInstance()["inicio"])
+   print(SingletonIdioma.getInstance()["hombres"])
+   print(SingletonIdioma.getInstance()["mujeres"])
+   print(SingletonIdioma.getInstance()["instagram"])
