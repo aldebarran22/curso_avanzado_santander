ad98cbd64502e43d0b76163514d7e99525728d58
aldebarran22 <antonioespinherranz@hotmail.com>
2024-09-23 13:21:21 +0200

    13.21

---
+++ codigo_sep_23_27/modelo/Model/Figura.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+class Figura:
+    def __init__(self):
+        self.color = None
+        self.etiqueta = None
+
---
+++ codigo_sep_23_27/modelo/Model/Triangulo.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+from Figura import Figura
+
+class Triangulo(Figura):
+    pass
