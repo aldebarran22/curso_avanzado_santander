97a33e220a2a512a934275200f0177fae3c2fe36
aldebarran22 <antonioespinherranz@hotmail.com>
2024-09-25 14:02:11 +0200

    14.02

--- codigo_sep_23_27/pandas_sql.py
+++ codigo_sep_23_27/pandas_sql.py
@@ -3,6 +3,7 @@ Extraer dataframes de una BD.
 """
 
 import sqlite3 as db
+import pandas as pd
 from os.path import isfile
 
 
@@ -14,6 +15,9 @@ class BaseDatos:
 
         self.con = db.connect(path)
 
+    def getDataFrame(self, sql):
+        return pd.read_sql(sql, self.con)
+
     def __del__(self):
         if hasattr(self, "con"):
             self.con.close()
@@ -22,5 +26,11 @@ class BaseDatos:
 if __name__ == "__main__":
     try:
         baseDatos = BaseDatos("../BBDD/empresa3.db")
+        sql = """select p.id as idprod, c.nombre, p.nombre, 
+        p.precio, p.existencias from productos p inner join
+        categorias c on p.idcategoria = c.id"""
+        df = BaseDatos.getDataFrame(sql)
+        print(df.head())
+
     except Exception as e:
         print(e)
