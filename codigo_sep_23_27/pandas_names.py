fcae282f0f20e560049486dc2cdb7d108e3bd6a4
aldebarran22 <antonioespinherranz@hotmail.com>
2024-09-25 12:51:05 +0200

    12.51

--- codigo_sep_23_27/pandas_names.py
+++ codigo_sep_23_27/pandas_names.py
@@ -6,9 +6,15 @@ import pandas as pd
 
 def cargaDF(año):
     path = f"../ficheros_curso/names/yob{año}.txt"
-    df = pd.read_csv(path)
+    df = pd.read_csv(path, header=None, names=["nombre","sexo","total"])    
     return df
 
+def sumar(año1, año2):
+    df1 = cargaDF(año1)
+    df2 = cargaDF(año2)
+    suma = df1 + df2
+    return suma
+
 if __name__ == '__main__':
-    df = cargaDF(2016)
-    print(df.head())
+    suma = sumar(2016, 2015)
+    print(suma.head())
