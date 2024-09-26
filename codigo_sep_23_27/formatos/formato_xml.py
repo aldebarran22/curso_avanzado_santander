243f61f4966dde1562f630d989bd3a7609b4a779
aldebarran22 <antonioespinherranz@hotmail.com>
2024-09-26 14:08:39 +0200

    14.08

--- codigo_sep_23_27/formatos/formato_xml.py
+++ codigo_sep_23_27/formatos/formato_xml.py
@@ -1,28 +1,28 @@
 """
 Generar XML con el contenido de la BD
 """
+
 from base_datos import Categoria, BaseDatos, path
 from xml.etree.ElementTree import Element, SubElement, Comment, tostring
 from xml.etree import ElementTree
 
+
 def generarXML():
     try:
         bd = BaseDatos(path)
-        raiz = Element('categorias')
-        raiz.set('version','1.0')
+        raiz = Element("categorias")
+        raiz.set("version", "1.0")
 
         listaCategorias = bd.selectCategorias()
         for c in listaCategorias:
-            categoria = SubElement(raiz, "categoria",\
-                                   {"idcat":str(c.id)})
+            categoria = SubElement(raiz, "categoria", {"idcat": str(c.id)})
             nombreCat = SubElement(categoria, "nombre")
             nombreCat.text = c.nombre
 
-            productos = SubElement(categoria, "productos")            
+            productos = SubElement(categoria, "productos")
             listaProductos = bd.select(c.nombre)
             for p in listaProductos:
-                producto = SubElement(productos, "producto", \
-                                      {"idprod":str(p.id)})
+                producto = SubElement(productos, "producto", {"idprod": str(p.id)})
                 nombreProd = SubElement(producto, "nombre")
                 nombreProd.text = p.nombre
 
@@ -31,14 +31,27 @@ def generarXML():
 
                 existenciasProd = SubElement(producto, "existencias")
                 existenciasProd.text = str(p.exis)
-        
+
         tree = ElementTree.ElementTree()
         tree._setroot(raiz)
-        tree.write('productos.xml')
-        print('Fichero generado: productos.xml')
+        tree.write("productos.xml")
+        print("Fichero generado: productos.xml")
 
     except Exception as e:
         print(e)
 
-if __name__ == '__main__':
-    generarXML()
+
+def procesarXML(path):
+    with open(path, "r") as f:
+        top = ElementTree.parse(f)
+        raiz = top.getroot()
+        # print(tostring(raiz))
+
+        # Extraer los nombres de las categorias:
+        for t in raiz.findall(".//categoria/nombre"):
+            print(t.tag, t.text)
+
+
+if __name__ == "__main__":
+    # generarXML()
+    procesarXML("productos.xml")
