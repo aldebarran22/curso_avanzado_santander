d33d26e58a3c913636ed5b3cea9cb0e0ebd272e6
aldebarran22 <antonioespinherranz@hotmail.com>
2024-09-26 11:48:52 +0200

    11.48

--- codigo_sep_23_27/java/ejemplo_jython/es/curso/app/modulo1.py
+++ codigo_sep_23_27/java/ejemplo_jython/es/curso/app/modulo1.py
@@ -5,11 +5,27 @@ Created on 26-sep-2024
 '''
 
 from java.util import ArrayList
+from es.curso.ejemplo_java import Permiso, Usuario
 
-if __name__ == '__main__':
+def pruebaArrayList():
     arr = ArrayList()
     
     for i in range(50):
         arr.add(i)
     
     print(arr[:5])
+    
+def pruebaClasesJava():
+    p1 = Permiso("fichero.xlsx", 'x')
+    p2 = Permiso("apuntes.pdf", 'r')
+    
+    user = Usuario("invitado", "1234")
+    user.addPermiso(p1)
+    user.addPermiso(p2)
+    
+    print(user)
+    
+
+if __name__ == '__main__':
+    # pruebaArrayList()
+    pruebaClasesJava()
