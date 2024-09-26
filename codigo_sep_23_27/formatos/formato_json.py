c808d9c6289d6e0fd974732e82fc87aaec405916
aldebarran22 <antonioespinherranz@hotmail.com>
2024-09-26 12:53:22 +0200

    12.53

--- codigo_sep_23_27/formatos/formato_json.py
+++ codigo_sep_23_27/formatos/formato_json.py
@@ -4,7 +4,18 @@ Carga de un fichero en Json
 import json
 
 def cargaFich(path):
-    pass
+    fich = None
+    try:
+        fich = open(path, "r")
+        objeto = json.load(fich)
+        return objeto
+    
+    except Exception as e:
+        print(e)
+
+    finally:
+        if fich: fich.close()
 
 if __name__ == '__main__':
-    cargaFich("Pedidos.json")
+    lista = cargaFich("Pedidos.json")
+    print(len(lista), " pedidos")
