82d3e01cf9f6debb7de8e901f5857998227da164
aldebarran22 <antonioespinherranz@hotmail.com>
2024-09-27 10:19:07 +0200

    10.19

--- codigo_sep_23_27/servicios_rest/servicioBD.py
+++ codigo_sep_23_27/servicios_rest/servicioBD.py
@@ -30,7 +30,7 @@ class RecursoBD(Resource):
         except Exception as e:
             abort(404, message=str(e))
 
-api.add_resource(RecursoBD, "/productos","/productos/","/productos/<param>")
+api.add_resource(RecursoBD, "/","/productos","/productos/","/productos/<param>")
 
 if __name__ == "__main__":
     app.run(debug=True, port=8000)
