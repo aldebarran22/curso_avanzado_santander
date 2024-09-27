286b3a96b34eccf61b5b11d731d9cdf8c4257bc1
aldebarran22 <antonioespinherranz@hotmail.com>
2024-09-27 10:08:39 +0200

    10.08

--- codigo_sep_23_27/servicios_rest/servicioBD.py
+++ codigo_sep_23_27/servicios_rest/servicioBD.py
@@ -11,16 +11,22 @@ api = Api(app)
 
 class RecursoBD(Resource):
 
-    def get(self):
+    def get(self, param=None):
         try:
             bd = BaseDatos(path)
-            L = bd.select()
-            return [prod.to_json() for prod in L]
+            if param is None:
+                L = bd.select()
+                return [prod.to_json() for prod in L]
+            
+            elif param.isnumeric():
+                id = int(param)
+                prod = bd.read(id)
+                return prod.to_json()
         
         except Exception as e:
             abort(404, str(e))
 
-api.add_resource(RecursoBD, "/productos")
+api.add_resource(RecursoBD, "/productos/<param>")
 
 if __name__ == "__main__":
     app.run(debug=True, port=8000)
