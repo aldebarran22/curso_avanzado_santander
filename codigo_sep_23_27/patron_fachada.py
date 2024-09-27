5a713489766511df4304064cf3b0157b331b4a2b
aldebarran22 <antonioespinherranz@hotmail.com>
2024-09-27 14:53:39 +0200

    14.53

---
+++ codigo_sep_23_27/patron_fachada.py
@@ -0,0 +1,34 @@
+"""
+Patrón fachada para acceder a funciones de C a bajo nivel
+"""
+
+from ctypes import *
+
+class C:
+    """Patron Fachada para crear un envoltorio de la lib de C"""
+
+    # Variables de clase
+    __libc = cdll.msvcrt
+
+    # Mapeo de funciones de C
+    __strchr = __libc.strchr
+    __strstr = __libc.strstr       
+
+    @staticmethod
+    def strchr(cad, char):
+        """Envoltorio de la funcion strchr de C"""
+        C.__strchr.restype = c_char_p
+        return C.__strchr(bytes(cad, 'utf-8'), ord(char))
+
+    @staticmethod
+    def strstr(cad, search):
+        """Envoltorio de la funcion strstr de C"""
+        C.__strstr.restype = c_char_p
+        return C.__strstr(bytes(cad, 'utf-8'), bytes(search, 'utf-8'))
+
+if __name__ == '__main__':
+    ptr = C.strchr("hola que tal", 'q')
+    print(ptr)
+
+    ptr = C.strstr("hola que tal", 'tal')
+    print(ptr)
