45016da707930a138339f3ee81a1bafb12861376
aldebarran22 <antonioespinherranz@hotmail.com>
2024-09-27 10:24:25 +0200

    10.24

--- codigo_sep_23_27/servicios_rest/cliente.py
+++ codigo_sep_23_27/servicios_rest/cliente.py
@@ -3,11 +3,32 @@ Cliente REST implementado con requests.
 """
 
 import requests
+import json
+
 
 def testHW():
     url = "http://localhost:8000"
     resp = requests.get(url)
     print("RESP: ", resp.json())
 
-if __name__ == '__main__':
-    testHW()
+
+def descargarJSON():
+    fich = None
+    try:
+        url = "http://localhost:8000/productos"
+        resp = requests.get(url)
+
+        fich = open("productos.json", "w")
+        json.dump(resp.json(), fich)
+        print("productos.json grabado!!")
+
+    except Exception as e:
+        print(e)
+    finally:
+        if fich:
+            fich.close()
+
+
+if __name__ == "__main__":
+    # testHW()
+    descargarJSON()
