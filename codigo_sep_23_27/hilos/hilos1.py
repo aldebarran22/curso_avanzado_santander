75a0c5d1095b18e9e71e02a0f3dee764c5457d84
aldebarran22 <antonioespinherranz@hotmail.com>
2024-09-27 11:56:44 +0200

    11.56

---
+++ codigo_sep_23_27/hilos/hilos1.py
@@ -0,0 +1,33 @@
+"""
+Generar varios hilos que lanzan números aleatorios.
+"""
+
+from threading import Thread
+from random import randint
+from time import sleep
+
+
+class Hilo(Thread):
+
+    def __init__(self, nombre, n=10):
+        Thread.__init__(self, name=nombre)
+        self.n = n
+
+    def run(self):
+        for i in range(self.n):
+            print(self.name, " num: ", randint(1, 30))
+            sleep(randint(1, 3))
+
+        print(self.name, " ha terminado!")
+
+
+if __name__ == "__main__":
+    hilos = []
+    nHilos = 5
+    for i in range(nHilos):
+        nombre = f"H-{i+1}"
+        hilo = Hilo(nombre, randint(5, 10))
+        hilo.start()
+        hilos.append(hilo)
+        
+    print("main terminado!")
