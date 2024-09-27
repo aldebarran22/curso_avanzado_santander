0cf0accead98aa85790fb70ed877e5a586f7f80c
aldebarran22 <antonioespinherranz@hotmail.com>
2024-09-27 13:33:54 +0200

    13.33

--- codigo_sep_23_27/hilos/descarga_url.py
+++ codigo_sep_23_27/hilos/descarga_url.py
@@ -6,24 +6,36 @@ from queue import Queue
 import urllib.request as urllib2
 
 
-def descargaURL(url):
-	f = None
-	numero = None
-	
-	try:
-		f = urllib2.urlopen(url)                
-		numero = int(f.read())
-		print (numero)
-		return numero
-	
-	except Exception as e:
-		print("ERROR: ", e)
+class Download(Thread):
+
+	def __init__(self, ini, fin):
+		Thread.__init__(self)
+		self.ini = ini
+		self.fin = fin
+
+	def run(self):
+		for i in range(self.ini, self.fin):
+			url = f"http://localhost:8000/fich{i}.txt"
+			print(url)
+			numero = self.descargaURL(url)
+			print(numero)
+
+	def descargaURL(self,url):
+		f = None
+		numero = None
 		
-	finally:
-		if f != None: f.close()
+		try:
+			f = urllib2.urlopen(url)                
+			numero = int(f.read())
+			print (numero)
+			return numero
 		
+		except Exception as e:
+			print("ERROR: ", e)
+			
+		finally:
+			if f != None: f.close()
+			
 
 if __name__=='__main__':
-	url = 'http://localhost:8000/fich10.txt'
-	num = descargaURL(url)
-	print('Numero : ', num)
+	pass
