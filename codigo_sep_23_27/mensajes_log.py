c1735a5237ae69374350dc6d3392a00ef253dd0f
aldebarran22 <antonioespinherranz@hotmail.com>
2024-09-27 14:11:36 +0200

    14.11

--- codigo_sep_23_27/mensajes_log.py
+++ codigo_sep_23_27/mensajes_log.py
@@ -10,6 +10,17 @@ del logger.
 import logging
 
 def testConsola():
+    # Por defecto muestra los mensajes de W, E y C
+    logging.debug("mensaje debug")
+    logging.info("mensaje info")
+    logging.warning("mensaje warning")
+    logging.error("mensaje error")
+    logging.critical("mensaje critical")
+
+def test2Consola():
+    # Establecer una configuración
+    logging.basicConfig(level=logging.DEBUG)
+
     logging.debug("mensaje debug")
     logging.info("mensaje info")
     logging.warning("mensaje warning")
@@ -17,6 +28,7 @@ def testConsola():
     logging.critical("mensaje critical")
 
 if __name__ == '__main__':
-    testConsola()
+    #testConsola()
+    test2Consola()
 
 
