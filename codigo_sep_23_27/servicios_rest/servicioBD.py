07876ed6d2bb05584fc7ab424135ba44b8effa37
aldebarran22 <antonioespinherranz@hotmail.com>
2024-09-27 10:46:25 +0200

    10.46

--- codigo_sep_23_27/servicios_rest/servicioBD.py
+++ codigo_sep_23_27/servicios_rest/servicioBD.py
@@ -3,13 +3,27 @@ Servicio REST con las operaciones CRUD a la BD
 """
 
 from flask import Flask
-from flask_restful import Resource , Api, abort
+from flask_restful import Resource , Api, abort, request
 from base_datos import BaseDatos, Producto, Categoria, path
 
 app = Flask(__name__)
 api = Api(app)
 
 class RecursoBD(Resource):
+
+    def post(self):
+        try:
+            bd = BaseDatos(path) 
+            args = request.json
+
+            # Crear un objeto producto:
+            producto = Producto.create(args)
+            n = bd.create(producto)
+            return {"create":n}
+
+        except Exception as e:
+            abort(404, message=str(e))
+
 
     def delete(self, param):
         try:
