7a86b11a48dc72ade2264ffbbe91b06549f6b73a
aldebarran22 <antonioespinherranz@hotmail.com>
2024-09-27 12:00:48 +0200

    12.00

--- codigo_sep_23_27/hilos/hilos1.py
+++ codigo_sep_23_27/hilos/hilos1.py
@@ -12,13 +12,17 @@ class Hilo(Thread):
     def __init__(self, nombre, n=10):
         Thread.__init__(self, name=nombre)
         self.n = n
+        self.L = list()
 
     def run(self):
         for i in range(self.n):
-            print(self.name, " num: ", randint(1, 30))
+            numero = randint(1, 30)
+            self.L.append(numero)
+            print(self.name, " num: ", numero)
             sleep(randint(1, 3))
 
         print(self.name, " ha terminado!")
+        print(self.L)
 
 
 if __name__ == "__main__":
@@ -29,5 +33,5 @@ if __name__ == "__main__":
         hilo = Hilo(nombre, randint(5, 10))
         hilo.start()
         hilos.append(hilo)
-        
+
     print("main terminado!")
