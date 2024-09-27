3132f3419f2fd4afe08d93641077b5c3939cddc9
aldebarran22 <antonioespinherranz@hotmail.com>
2024-09-27 13:48:11 +0200

    13.48

--- codigo_sep_23_27/hilos/descarga_url.py
+++ codigo_sep_23_27/hilos/descarga_url.py
@@ -9,16 +9,18 @@ import urllib.request as urllib2
 
 class Download(Thread):
 
-    def __init__(self, ini, fin):
+    def __init__(self, ini, fin, q):
         Thread.__init__(self)
         self.ini = ini
         self.fin = fin
+        self.q = q
 
     def run(self):
         for i in range(self.ini, self.fin):
             url = f"http://localhost:8000/fich{i}.txt"
             numero = self.descargaURL(url)
             print(self.name, url, numero)
+            self.q.put(numero)
 
     def descargaURL(self, url):
         f = None
@@ -45,11 +47,16 @@ if __name__ == "__main__":
     params = [
         (i * num_fich_hilo, i * num_fich_hilo + num_fich_hilo) for i in range(num_hilos)
     ]
-
+    q = Queue(num_ficheros)
     for i in range(num_hilos):
-        h = Download(*params[i])
+        h = Download(*params[i], q)
         h.start()
         hilos.append(h)
 
     for h in hilos:
         h.join()
+    L = list()
+    while not q.empty():
+        L.append(q.get())
+    L.sort()
+    print(L[:10])
