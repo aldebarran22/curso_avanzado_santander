39f3c119c026b53a90560e77ea5abfacbb2cfe29
aldebarran22 <antonioespinherranz@hotmail.com>
2024-12-09 10:10:15 +0100

    10.10

--- codigo1/csv_dict.py
+++ codigo1/csv_dict.py
@@ -4,16 +4,26 @@ Cargar ficheros csv y convertirlos a diccionarios
 
 def cargarFichero(path):
     fich = open(path, 'r')
-    txt = fich.read()
+    txt = fich.read().strip()
     fich.close()
     return txt
 
 
 def csvToDict(texto, sep=';'):
-    pass
+    lineas = texto.split("\n")
+    cabs = lineas[0].split(sep)
+    resul = []
+    #print(cabs)
+    for linea in lineas[1:]:
+        datos = linea.split(sep)
+        #print(datos)
+        dicc = dict(zip(cabs, datos))
+        #print(dicc)
+        resul.append(dicc)
+    return resul
 
 if __name__ == '__main__':
-    txt = cargarFichero("../ficheros_curso/merge/Empleados.txt")
+    txt = cargarFichero("../ficheros_curso/merge/Pedidos.txt")
     print(txt)
     dicc = csvToDict(txt)
     print(dicc)
