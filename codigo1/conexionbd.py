1c871210eb0851e01dff4b2bdbfdb01d335a9472
aldebarran22 <antonioespinherranz@hotmail.com>
2024-12-09 12:37:53 +0100

    12.37

--- codigo1/conexionbd.py
+++ codigo1/conexionbd.py
@@ -6,23 +6,18 @@ Utilizar el destructor para cerrar la conexión.
 import sqlite3 as db
 from os.path import isfile
 
+
 class Conexion:
 
     def __init__(self, path):
         if not isfile(path):
-            raise FileNotFoundError(f"el fichero: {path} no existe")        
+            raise FileNotFoundError(f"el fichero: {path} no existe")
         else:
             self.con = db.connect(path)
-
 
     def __del__(self):
         self.con.close()
 
-    
-if __name__ == '__main__':
-    conexion = Conexion('empresa3.db')
 
-
-
-
-
+if __name__ == "__main__":
+    conexion = Conexion("./empresa3.db")
