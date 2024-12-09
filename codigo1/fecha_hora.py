e7f2a6edf91c61afe83964e14f5f5db1de9e982c
aldebarran22 <antonioespinherranz@hotmail.com>
2024-12-09 12:22:35 +0100

    12.22

--- codigo1/fecha_hora.py
+++ codigo1/fecha_hora.py
@@ -5,14 +5,24 @@ class Time:
     def __init__(self,hh=0,mm=0,ss=0):
         self.hh = hh
         self.mm = mm
-        self.ss = ss
-
-   
+        self.ss = ss 
    
     def __str__(self):  
         # 1, 2, 33 => 01:02:33      
         return '%02d:%02d:%02d' % (self.hh,self.mm,self.ss)
 
+    def __add__(self, other):
+        nuevo = Time(self.hh+other.hh,self.mm+other.mm, self.ss+other.ss)
+        nuevo.__ajustar()
+        return nuevo
+    
+    def __ajustar(self):
+        minutos = self.ss // 60
+        self.ss %= 60
+        self.mm += minutos
+        horas = self.mm // 60
+        self.mm %= 60
+        self.hh += horas
 
    
 
