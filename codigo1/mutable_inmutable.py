66f83d27acfe2d20d9ae4c514f59515dbd1c425f
aldebarran22 <antonioespinherranz@hotmail.com>
2024-12-09 09:49:16 +0100

    9.49

--- codigo1/mutable_inmutable.py
+++ codigo1/mutable_inmutable.py
@@ -13,3 +13,37 @@ cad = "hola"
 print(cad)
 print(cad.upper())
 print(cad)
+
+# Copia de objetos mutables:
+L1 = [3,4,5,2,1]
+L2 = L1 # ojo, es una referencia.
+L1[0] = 99
+print("L1", L1, id(L1))
+print("L2", L2, id(L2))
+print()
+
+L1 = [3,4,5,2,1]
+L2 = L1.copy() # ojo, es una referencia.
+L1[0] = 99
+print("L1", L1, id(L1))
+print("L2", L2, id(L2))
+
+print()
+
+L1 = [[3,4],[5,2,1]]
+L2 = L1.copy() # ojo, es una copia superficial.
+L1[0] = 99
+L1[-1].append(999)
+print("L1", L1, id(L1))
+print("L2", L2, id(L2))
+
+
+print()
+
+import copy
+L1 = [[3,4],[5,2,1]]
+L2 = copy.deepcopy(L1) # es una copia profunda
+L1[0] = 99
+L1[-1].append(999)
+print("L1", L1, id(L1))
+print("L2", L2, id(L2))
