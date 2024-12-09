72d3ed2db4b7ce091943ef490b3041b9597a4197
aldebarran22 <antonioespinherranz@hotmail.com>
2024-12-09 09:36:29 +0100

    9.36

---
+++ codigo1/mutable_inmutable.py
@@ -0,0 +1,15 @@
+"""
+Comparativa entre un tipo mutable e inmutable
+"""
+
+# Objeto mutable:
+L = [3,5,6,2,1,3,7]
+print(L)
+L.sort()
+print(L)
+
+# Objeto inmutable:
+cad = "hola"
+print(cad)
+print(cad.upper())
+print(cad)
