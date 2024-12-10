b37def1cc4e691e3e89f50f78c87d120d3b2ac2e
aldebarran22 <antonioespinherranz@hotmail.com>
2024-12-10 10:23:12 +0100

    10.23

--- codigo2/csv_to_json.py
+++ codigo2/csv_to_json.py
@@ -2,29 +2,40 @@
 Conversor de CSV a Json
 """
 
+
 def cargarCSV(path):
-    fich = open(path, 'r')
-    txt = fich.read()
+    fich = open(path, "r")
+    txt = fich.read().strip()
     fich.close()
     return txt
 
+
 def csvJson(txt, sep=";"):
-    lineas = txt.split('\n')
+    lineas = txt.split("\n")
     cabs = lineas[0].split(sep)
-    #print(cabs)
+    # print(cabs)
     resul = []
     for fila in lineas[1:]:
         valores = fila.split(sep)
         dicc = dict(zip(cabs, valores))
         resul.append(dicc)
-        #print(dicc)
+        # print(dicc)
     return resul
 
 
-if __name__ == '__main__':
+def jsonCsv(datos, sep=";"):
+    """
+    Recibe una lista de dicc. y los convierte a un formato CSV
+    """
+    cabs = sep.join(datos[0].keys())
+    print(cabs)
+
+
+if __name__ == "__main__":
     fichero = "Empleados"
     path = f"../ficheros_curso/merge/{fichero}.txt"
     txt = cargarCSV(path)
     print(txt)
-    csvJson(txt)
-
+    datos = csvJson(txt)
+    print(datos)
+    jsonCsv(datos)
