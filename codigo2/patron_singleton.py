5881e311df9529dedf9c8b7c8f4d16ca6833dbe2
aldebarran22 <antonioespinherranz@hotmail.com>
2024-12-10 14:59:21 +0100

    15.00

--- codigo2/patron_singleton.py
+++ codigo2/patron_singleton.py
@@ -5,11 +5,16 @@ Patrón singleton para implementar un traductor de etiquetas
 def cargarIdioma(idioma):
     path = f"../ficheros_curso/idiomas/{idioma}.txt"
     fich = open(path, 'r')
+    dicc = dict()
     for linea in fich:
-        print(linea)
+        linea = linea.rstrip()
+        k, _, v = linea.partition('=')
+        dicc[k] = v        
     fich.close()
+    return dicc
 
 
 if __name__ == '__main__':
-    cargarIdioma('en')
+    dicc = cargarIdioma('it')
+    print(dicc)
 
