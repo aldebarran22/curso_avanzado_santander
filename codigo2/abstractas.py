28de35343d3cfc1d568dc6acaf783b95ee00e536
aldebarran22 <antonioespinherranz@hotmail.com>
2024-12-10 14:27:52 +0100

    14.27

--- codigo2/abstractas.py
+++ codigo2/abstractas.py
@@ -17,5 +17,15 @@ class Figura(abc.ABC):
     def dibujar(self):
         pass
 
+class Circulo(Figura):
+
+    def __init__(self, color="black", texto="", radio=5):
+        Figura.__init__(self, color, texto)
+        self.radio = radio
+
+    def dibujar(self):
+        print(self.color, self.texto, self.radio)
+
 if __name__ == "__main__":
-    fig = Figura("red", "etiqueta")
+    fig = Circulo("red", "etiqueta", 10)
+    fig.dibujar()
