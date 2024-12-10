d47c1fa5ba2b652daed7e7226eb8f94b7b07650b
aldebarran22 <antonioespinherranz@hotmail.com>
2024-12-10 12:53:27 +0100

    12.53

--- codigo2/base_datos.py
+++ codigo2/base_datos.py
@@ -2,15 +2,23 @@
 Conexión a la BD con control de excepciones
 """
 
-import sqlite3
+import sqlite3 as db
+from os.path import isfile
 
 class BaseDatos:
     
     def __init__(self, path):
-        pass
+        if not isfile(path):
+            raise FileNotFoundError(f"No existe el fichero: {path}")
+        else:
+            self.con = db.connect(path)
 
     def __del__(self):
-        pass
+        self.con.close()
 
 if __name__ == '__main__':
-    bd = BaseDatos("../BBDD/empresa3.db")
+    try:
+        bd = BaseDatos("../BBDD/empresa3.db")
+        
+    except Exception as e:
+        print(e)
