377a554376a6a44762027727f18a46a26038aa46
aldebarran22 <antonioespinherranz@hotmail.com>
2024-12-10 11:32:35 +0100

    11.32

---
+++ codigo2/objetos.py
@@ -0,0 +1,17 @@
+"""
+POO en Python
+"""
+
+class Pedido:
+    """
+    Implementación de la clase Pedido
+    """
+
+    def __init__(self, idpedido=0, idcliente='', importe=0.0, pais=''):
+        self.idpedido = idpedido
+        self.idcliente = idcliente
+        self.importe = importe
+        self.pais = pais
+
+    
+
