11cc34f551b1a65b9c96c82ed408c84fe402d4ca
aldebarran22 <antonioespinherranz@hotmail.com>
2024-12-10 13:18:35 +0100

    13.18

--- codigo2/fecha_hora.py
+++ codigo2/fecha_hora.py
@@ -11,7 +11,18 @@ class Time:
         return '%02d:%02d:%02d' % (self.hh,self.mm,self.ss)
     
     def __add__(self, other):
-        pass
+        nuevo = Time(self.hh+other.hh,self.mm+other.mm,self.ss+other.ss)
+        nuevo.__ajustar()
+        return nuevo
+    
+    def __ajustar(self):
+        minutos = self.ss // 60
+        self.ss %= 60
+        self.mm += minutos
+
+        horas = self.mm // 60
+        self.mm %= 60
+        self.hh += horas
         
 class Date:
 
