c72f351b714dfa81bce91d56c7a0e141d6a8d85e
aldebarran22 <antonioespinherranz@hotmail.com>
2024-12-10 11:37:12 +0100

    11.37

--- codigo2/objetos.py
+++ codigo2/objetos.py
@@ -13,5 +13,13 @@ class Pedido:
         self.importe = importe
         self.pais = pais
 
-    
+    def __str__(self):
+        return str(self.idpedido) + ' ' + self.idcliente + ' ' + str(self.importe) + ' ' + self.pais
+
+
+if __name__ == '__main__':
+    ped1 = Pedido(10440, 'ALFKI', 23.56, 'España')
+    print(ped1)
+    print(str(ped1))
+    print(ped1.__str__())
 
