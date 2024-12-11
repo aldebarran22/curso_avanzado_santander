73051ef5320a3994a9fe9e38ae08028967ec3181
aldebarran22 <antonioespinherranz@hotmail.com>
2024-12-11 09:11:52 +0100

    9.11

--- codigo1/patron_singleton/traductor.py
+++ codigo1/patron_singleton/traductor.py
@@ -3,4 +3,20 @@ Implementación del patrón singleton para la traducción de etiquetas
 """
 
 def cargaFichero(idioma):
-    path = 
+    fich = None
+    try:
+        path = f"idiomas/{idioma}.txt"
+        fich = open(path, 'r')
+        for fila in fich:
+            print(fila)
+
+
+    except  Exception as e:
+        print(e)
+
+    finally:
+        if fich:
+            fich.close()
+
+if __name__ == '__main__':
+    cargaFichero('es')
