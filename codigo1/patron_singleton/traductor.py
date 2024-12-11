3621906b5f53169103b31e78d90e74d9e5db94cc
aldebarran22 <antonioespinherranz@hotmail.com>
2024-12-11 09:36:45 +0100

    9.36

--- codigo1/patron_singleton/traductor.py
+++ codigo1/patron_singleton/traductor.py
@@ -1,29 +1,43 @@
 """
 Implementación del patrón singleton para la traducción de etiquetas
 """
+class SingletonIdioma:
 
-def cargaFichero(idioma):
-    fich = None
-    dicc = dict()
-    try:
-        path = f"idiomas/{idioma}.txt"
-        fich = open(path, 'r')
-        for fila in fich:
-            fila = fila.rstrip()
-            k, _, v = fila.partition('=')
-            dicc[k] = v
+    __diccionario = None
 
-        return dicc
-    except  Exception as e:
-        raise ValueError(f'El idioma {idioma} no está soportado')
+    @staticmethod
+    def getInstance(idioma):
 
-    finally:
-        if fich:
-            fich.close()
+        if SingletonIdioma.__diccionario is None:
+            # Cargar el idioma
+            SingletonIdioma.__diccionario = SingletonIdioma.__cargaFichero(idioma)
+        
+        return SingletonIdioma.__diccionario
+        
+
+    @staticmethod
+    def __cargaFichero(idioma):
+        fich = None
+        dicc = dict()
+        try:
+            path = f"idiomas/{idioma}.txt"
+            fich = open(path, 'r')
+            for fila in fich:
+                fila = fila.rstrip()
+                k, _, v = fila.partition('=')
+                dicc[k] = v
+
+            return dicc
+        except  Exception as e:
+            raise ValueError(f'El idioma {idioma} no está soportado')
+
+        finally:
+            if fich:
+                fich.close()
 
 if __name__ == '__main__':
     try:
-        d = cargaFichero('it')
+        d = SingletonIdioma.getInstance("en")['inicio']
         print(d)
     except  Exception as e:
         print(e)
