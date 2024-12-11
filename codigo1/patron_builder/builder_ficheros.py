77ea42e1ab7020de9d75e2730ef5fecc019e2f27
aldebarran22 <antonioespinherranz@hotmail.com>
2024-12-11 12:02:15 +0100

    12.02

--- codigo1/patron_builder/builder_ficheros.py
+++ codigo1/patron_builder/builder_ficheros.py
@@ -5,62 +5,82 @@ a distintos formatos: XML, HTML, JSON
 
 import abc
 
+
 class Builder(abc.ABC):
 
     @abc.abstractmethod
-    def crearCabecera(self,L):
+    def crearCabecera(self, L):
         pass
 
     @abc.abstractmethod
-    def crearDetalle(self,L):
+    def crearDetalle(self, L, **kwargs):
         pass
+
 
 class BuilderHTML(Builder):
 
-    def crearCabecera(self,L):
+    def crearCabecera(self, L):
         cabecera = "".join([f"<th>{col}</th>" for col in L])
         return "<tr>" + cabecera + "</tr>"
-   
-    def crearDetalle(self,L):
+
+    def crearDetalle(self, L, **kwargs):
         detalle = "".join([f"<td>{col}</td>" for col in L])
         return "<tr>" + detalle + "</tr>"
 
+
 class BuilderJSON(Builder):
     pass
+
 
 class BuilderXML(Builder):
 
     def __init__(self):
         self.cabeceras = None
-    
-    def crearCabecera(self,L):
+
+    def crearCabecera(self, L):
         self.cabeceras = L
         return ""
 
-    def crearDetalle(self,L):
-        pass
+    def crearDetalle(self, L, **kwargs):
+        detalle = ""
+        etiqueta = kwargs['etiqueta']
+        for i, col in enumerate(L):
+            cab = self.cabeceras[i]
+            detalle += f"<{cab}>{col}</{cab}>"
+        return f"<{etiqueta}>{detalle}</{etiqueta}>"
+
 
 class Director:
 
     def __init__(self, builder):
         self.builder = builder
+        self.nombreFichero = ""
+        self.etiqueta = ""
 
-    def convertirFormato(self, path, sep=';'):
+    def extraerEtiqueta(self, path):
+        L = path.split("/")
+        fichero = L[-1]
+        t = fichero.partition('.')
+        self.nombreFichero = t[0]
+        self.etiqueta = t[0][:-1].lower()
+
+    def convertirFormato(self, path, sep=";"):
         """Va leyendo el fichero y convirtiendo el CSV al formato
         para ello utiliza el builder"""
         fich = None
         cabs = True
         tabla = ""
         try:
-            fich = open(path, 'r')
+            self.extraerEtiqueta(path)
+            fich = open(path, "r")
             for linea in fich:
                 linea = linea.rstrip()
-                L = linea.split(sep)                
+                L = linea.split(sep)
                 if cabs:
-                    tabla += self.builder.crearCabecera(L)                    
+                    tabla += self.builder.crearCabecera(L)
                     cabs = False
                 else:
-                    tabla += self.builder.crearDetalle(L)
+                    tabla += self.builder.crearDetalle(L, etiqueta=self.etiqueta)
 
             print(tabla)
 
@@ -70,9 +90,9 @@ class Director:
             if fich:
                 fich.close()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
+    #builder = BuilderXML()
     builder = BuilderHTML()
     director = Director(builder)
-    director.convertirFormato("Pedidos.txt")
-
-
+    director.convertirFormato("Empleados.txt")
