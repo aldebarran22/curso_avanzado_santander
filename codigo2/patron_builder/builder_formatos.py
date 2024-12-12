e61c835e0f7db76f8fd07b97f7ab758429c812dd
aldebarran22 <antonioespinherranz@hotmail.com>
2024-12-12 12:54:19 +0100

    12.54

--- codigo2/patron_builder/builder_formatos.py
+++ codigo2/patron_builder/builder_formatos.py
@@ -9,10 +9,11 @@ de un fichero CSV a:
 import abc
 import json
 
+
 class Builder(abc.ABC):
 
     @abc.abstractmethod
-    def generarCabecera(self, L):
+    def generarCabecera(self, L, **kwargs):
         pass
 
     @abc.abstractmethod
@@ -23,20 +24,19 @@ class Builder(abc.ABC):
     def grabarFichero(self, tabla, path):
         pass
 
+
 class BuilderHTML(Builder):
 
-    def generarCabecera(self, L):
+    def generarCabecera(self, L, **kwargs):
         cabeceras = ""
         for col in L:
             cabeceras += f"<th>{col}</th>"
         return f"<tr>{cabeceras}</tr>"
 
-    
     def generarDetalle(self, L):
         detalle = "".join([f"<td>{campo}</td>" for campo in L])
         return f"<tr>{detalle}</tr>"
 
-   
     def grabarFichero(self, tabla, path):
         pathPlantilla = "plantilla.html"
         pathFichero = path + ".html"
@@ -54,7 +54,7 @@ class BuilderHTML(Builder):
 
         except Exception as e:
             raise e
-        
+
         finally:
             if fichPlantilla:
                 fichPlantilla.close()
@@ -63,48 +63,46 @@ class BuilderHTML(Builder):
                 fichOut.close()
 
 
-
-
 class BuilderXML(Builder):
 
     def __init__(self):
         self.cabeceras = ""
-    
-    def generarCabecera(self, L):
+
+    def generarCabecera(self, L, **kwargs):
         self.cabeceras = L
         return ""
-    
+
     def generarDetalle(self, L):
         detalle = ""
         for pos, dato in enumerate(L):
             detalle += f"<{self.cabeceras[pos]}>{dato}</{self.cabeceras[pos]}>"
         return detalle
 
-   
     def grabarFichero(self, tabla, path):
-        pass
+        print(tabla)
+
 
 class BuilderJSON(Builder):
-    
+
     def __init__(self):
         self.cabeceras = ""
         self.lista = []
-    
-    def generarCabecera(self, L):
+
+    def generarCabecera(self, L, **kwargs):
         self.cabeceras = L
         return ""
-    
+
     def generarDetalle(self, L):
         dicc = dict(zip(self.cabeceras, L))
         self.lista.append(dicc)
         return ""
 
-   
     def grabarFichero(self, tabla, path):
         pathFichero = path + ".json"
         fich = open(pathFichero, "w")
         json.dump(self.lista, fich, indent=4)
         fich.close()
+
 
 class Director:
 
@@ -112,23 +110,32 @@ class Director:
         self.builder = builder
         self.tabla = ""
         self.pathDestino = pathDestino
+        self.etiquetaPrincipal = ""
+        self.etiqueta = ""
 
     def __construirPath(self, pathOrigen):
         L = pathOrigen.split("/")
         fichero = L[-1]
-        return self.pathDestino + "/" + fichero.partition('.')[0]
+        nombreFichero = fichero.partition(".")[0]
+        self.etiquetaPrincipal = nombreFichero.lower()
+        self.etiqueta = nombreFichero[:-1].lower()
+        return self.pathDestino + "/" + nombreFichero
 
-    def build(self, pathOrigen, sep=';'):
+    def build(self, pathOrigen, sep=";"):
         fich = None
         cabs = True
-        pathFichero = self.__construirPath(pathOrigen) 
+        pathFichero = self.__construirPath(pathOrigen)
         try:
-            fich = open(pathOrigen, 'r')
+            fich = open(pathOrigen, "r")
             for fila in fich:
                 fila = fila.rstrip()
                 L = fila.split(sep)
                 if cabs:
-                    self.tabla += self.builder.generarCabecera(L)
+                    self.tabla += self.builder.generarCabecera(
+                        L,
+                        etiquetaPrincipal=self.etiquetaPrincipal,
+                        etiqueta=self.etiqueta
+                    )
                     cabs = False
                 else:
                     self.tabla += self.builder.generarDetalle(L)
@@ -142,9 +149,9 @@ class Director:
                 fich.close()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     try:
-        builder = BuilderHTML()
+        builder = BuilderXML()
         director = Director(builder, "destino")
         director.build("origen/Empresas.txt")
     except Exception as e:
