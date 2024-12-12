63fa3865f2c8a8cb50fe826a3d658d032703e2e9
aldebarran22 <antonioespinherranz@hotmail.com>
2024-12-12 09:30:49 +0100

    9.30

--- codigo2/patron_singleton/i18n.py
+++ codigo2/patron_singleton/i18n.py
@@ -3,10 +3,15 @@
 class Singletoni18n:
 
     __instance = None
+    __idioma = None
 
     @staticmethod
     def getInstance(idioma):
-        pass
+        if Singletoni18n.__instance == None or Singletoni18n.__idioma != idioma:
+            print('Se ha cargado el fichero de ', idioma)
+            Singletoni18n.__instance = Singletoni18n.__cargarIdioma(idioma)
+        
+        return Singletoni18n.__instance
     
     @staticmethod
     def __cargarIdioma(idioma):
