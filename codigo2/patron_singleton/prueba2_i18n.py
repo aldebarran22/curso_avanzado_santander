6ca57ae12b90f55667aa875d3b38686506274d00
aldebarran22 <antonioespinherranz@hotmail.com>
2024-12-12 09:38:04 +0100

    9.38

---
+++ codigo2/patron_singleton/prueba2_i18n.py
@@ -0,0 +1,5 @@
+from i18n import Singletoni18n
+
+
+def funcion3():
+    print(Singletoni18n.getInstance("en")['instagram'])
--- codigo2/patron_singleton/prueba_i18n.py
+++ codigo2/patron_singleton/prueba_i18n.py
@@ -1,5 +1,6 @@
 
 from i18n import Singletoni18n
+from prueba2_i18n import funcion3
 
 def funcion1():
     print(Singletoni18n.getInstance("es")['twitter'])
@@ -10,3 +11,4 @@ if __name__ == '__main__':
     print(Singletoni18n.getInstance("es")['inicio'])
     print(Singletoni18n.getInstance("es")['facebook'])
     funcion1()
+    funcion3()
