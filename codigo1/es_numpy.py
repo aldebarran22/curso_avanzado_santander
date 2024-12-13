b485059fbd70e117ece7cae723efa68e9c24ad04
aldebarran22 <antonioespinherranz@hotmail.com>
2024-12-13 10:51:16 +0100

    10.51

--- codigo1/es_numpy.py
+++ codigo1/es_numpy.py
@@ -17,9 +17,26 @@ def grabarTxt(m, path):
 def cargarTxt(path):
     return np.loadtxt(path, delimiter=';', dtype="u1")
 
+def grabar2(path, *matrices):
+    #dicc = {"m"+str(i+1):m for i, m in enumerate(matrices)}
+    np.savez_compressed(path, m1=matrices[0], m2=matrices[1], m3=matrices[2])
+
+def load2(path):
+    matrices = np.load(path)
+    print("--------------------------")
+    print(matrices['m2'])
+    print()
+
 if __name__ == '__main__':
     m = np.random.randint(1,100, (10, 15))
+    m2 = np.random.randint(1,100, (5, 5))
+    m3 = np.random.randint(1,100, (10, 10))
+
     grabarTxt(m, "../ficheros/matriz.csv")
-    m2 = cargarTxt("../ficheros/matriz.csv")
-    print(m2)
+    mCargada = cargarTxt("../ficheros/matriz.csv")
+    print(mCargada)
+
+    grabar2("../ficheros/matriz.npz", m, m2, m3)
+    load2("../ficheros/matriz.npz")
+
     
