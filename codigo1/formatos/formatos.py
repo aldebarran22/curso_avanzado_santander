ba3516e8eb28a4076417178ae15d40e151b29182
aldebarran22 <antonioespinherranz@hotmail.com>
2024-12-17 11:40:50 +0100

    11.40

--- codigo1/formatos/formatos.py
+++ codigo1/formatos/formatos.py
@@ -19,9 +19,21 @@ def generarXML(L, path):
     # Generar un producto por cada objeto de la colección:
     for p in L:
         nodoProducto = SubElement(raiz, 'producto')
+        nodoProducto.attrib['id'] = str(p.id)
+        nodoNombre = SubElement(nodoProducto, 'nombre')
+        nodoNombre.text = p.nombre
+        nodoPrecio = SubElement(nodoProducto, 'precio')
+        nodoPrecio.text = str(round(p.precio,2))
+        nodoCat = SubElement(nodoProducto, 'categoria')
+        nodoCat.attrib['idcat']=str(p.cat.id)
+        nodoCat.text = p.cat.nombre
+        nodoExistencias = SubElement(nodoProducto, 'existencias')
+        nodoExistencias.text = str(p.exis)
+
+
 
     # Imprimir el árbol
-    print(tostring(raiz))
+    print(tostring(raiz, encoding='unicode'))
 
 if __name__ == "__main__":
     try:
