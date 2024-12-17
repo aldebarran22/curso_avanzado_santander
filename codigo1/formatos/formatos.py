246f855368c547c15413d6d8954c7e85d4876a71
aldebarran22 <antonioespinherranz@hotmail.com>
2024-12-17 11:26:48 +0100

    11.26

--- codigo1/formatos/formatos.py
+++ codigo1/formatos/formatos.py
@@ -4,11 +4,24 @@ Serialización de objetos con: pickle, shelve
 """
 
 from base_datos import Producto, Categoria, BaseDatos
-
+from xml.etree.ElementTree import Element, SubElement, tostring, Comment
 
 def generarXML(L, path):
-    pass
 
+    # Generar el nodo raiz:
+    raiz = Element("productos")
+    raiz.set("version", "1.0")
+    comentario = Comment("Productos extraidos de la base de datos Empresa3.db")
+
+    # Añadir el comentario al nodo raiz:
+    raiz.append(comentario)
+
+    # Generar un producto por cada objeto de la colección:
+    for p in L:
+        nodoProducto = SubElement(raiz, 'producto')
+
+    # Imprimir el árbol
+    print(tostring(raiz))
 
 if __name__ == "__main__":
     try:
