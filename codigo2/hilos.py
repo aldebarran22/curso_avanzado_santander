dab12cd86270d053a0739a7f753ceda93a20f42b
aldebarran22 <antonioespinherranz@hotmail.com>
2024-12-18 13:53:57 +0100

    13.53

---
+++ codigo2/hilos.py
@@ -0,0 +1,30 @@
+"""
+Crear varios hilos y almacenarlos en una lista.
+Uso de join()
+"""
+
+from threading import Thread
+from random import randint
+from time import sleep
+
+
+class Mensaje(Thread):
+
+    def __init__(self, n, nombre):
+        Thread.__init__(self, name=nombre)
+        self.n = n
+
+    def run(self):
+        for i in range(self.n):
+            print(self.name, "MENSAJE: ", (i + 1))
+            sleep(randint(1, 3))
+        print("Termina hilo: ", self.name)
+
+
+if __name__ == "__main__":
+    L = []
+    numHilos = 4
+    for i in range(numHilos):
+        hilo = Mensaje(randint(5, 10), f"H-{i+1}")
+        L.append(hilo)
+        hilo.start()
