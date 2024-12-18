80f57bfd34f11e886a75c9ca4241fb804e4b8ffc
aldebarran22 <antonioespinherranz@hotmail.com>
2024-12-18 09:25:21 +0100

    9.25

--- codigo2/formatos/formatos.py
+++ codigo2/formatos/formatos.py
@@ -12,14 +12,25 @@ import json
 
 def exportarJson(L, path):
     """Exportar todos los productos con la categoría a un fichero json"""
-    pass
+    fich = None
+    try:
+        fich = open(path, 'w')
+        R = [objeto.to_json() for objeto in L]
+        json.dump(R, fich, indent=4)
+        print(f'Se ha generado el fichero: {path}')
+        
+    except Exception as e:
+        raise e
+    
+    finally:
+        if fich:fich.close()
 
 if __name__ == '__main__':
     try:
         bd = BaseDatos("empresa3.db")
         L = bd.select()
         exportarJson(L, "productos.json")
-        
+
     except Exception as e:
         print(e)
     
