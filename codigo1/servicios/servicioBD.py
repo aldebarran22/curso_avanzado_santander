cc4d7979257c3458b50217be0bdb31bf961c45bd
aldebarran22 <antonioespinherranz@hotmail.com>
2024-12-19 10:32:42 +0100

    10.32

--- codigo1/servicios/servicioBD.py
+++ codigo1/servicios/servicioBD.py
@@ -21,15 +21,22 @@ class RecursoProducto(Resource):
                 L = bd.select()
                 return [p.to_json() for p in L]
             
-            else:
+            elif id.isnumeric():
+                # Comprobar si es número entero, si lo es: cargar un producto por id
+                id = int(id) 
                 producto = bd.read(id)
                 return producto.to_json()
+            
+            else:
+                # Lo tomamos como si fuera el nombre de la categoria:
+                L = bd.select(id)
+                return [p.to_json() for p in L]
 
         except Exception as e:
             abort(404, message=str(e))
 
 
-api.add_resource(RecursoProducto, "/productos","/productos/<id>")
+api.add_resource(RecursoProducto, "/productos","/productos/","/productos/<id>")
 
 if __name__ == "__main__":
     app.run(debug=True)
