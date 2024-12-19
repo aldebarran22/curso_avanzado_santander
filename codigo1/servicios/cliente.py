51837f4479c2acdf4927219b94b7b44fda6517d6
aldebarran22 <antonioespinherranz@hotmail.com>
2024-12-19 12:13:09 +0100

    12.13

--- codigo1/servicios/cliente.py
+++ codigo1/servicios/cliente.py
@@ -3,8 +3,9 @@ Implementación de un cliente Rest con la librería
 requests.
 """
 
-from requests import get, delete
+from requests import get, delete, post, put
 import json
+from base_datos import Categoria, Producto
 
 
 def testHelloWorld():
@@ -27,8 +28,19 @@ def testDeleteProductos(id):
     resp = delete(url)
     print(resp.json())
 
+def testPostProductos():
+    url = "http://localhost:5000/productos"
+
+    # Crear un objeto producto:
+    nuevo = Producto(150, "Red Bull 3", Categoria(1), 1.77, 100)
+    dicc = nuevo.to_json()
+    cabs = {"Content-type": "application/json"}
+    resp = post(url, data=json.dumps(dicc), headers=cabs)
+    print(resp.json())
+     
 
 if __name__ == "__main__":
     # testHelloWorld()
     # testGetProductos()
-    testDeleteProductos(24)
+    # testDeleteProductos(24)
+    testPostProductos()
--- codigo1/servicios/servicioBD.py
+++ codigo1/servicios/servicioBD.py
@@ -3,7 +3,7 @@ Servicio de Rest con las operaciones CRUD a la BD.
 """
 
 from flask import Flask
-from flask_restful import Resource, Api, abort
+from flask_restful import Resource, Api, abort, request
 from base_datos import BaseDatos, Categoria, Producto
 
 app = Flask(__name__)
@@ -46,6 +46,20 @@ class RecursoProducto(Resource):
 
         except Exception as e:
             abort(404, menssage=str(e))
+
+
+    def post(self):
+        try:
+            bd = BaseDatos(path)
+            args = request.json
+
+            # Tenemos que crear un objeto producto a partir del dicc en json
+            producto = Producto.create(args)
+            n = bd.create(producto) # Crearlo en la BD.
+            return {"create":n}
+
+        except Exception as e:
+            abort(404, message=str(e))
 
 
 api.add_resource(RecursoProducto, "/productos", "/productos/", "/productos/<id>")
