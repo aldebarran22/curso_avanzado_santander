36484219ab91492fc31a15c0342c270b074b0bd0
aldebarran22 <antonioespinherranz@hotmail.com>
2024-12-20 10:18:37 +0100

    10.18

--- codigo2/servicios/cliente.py
+++ codigo2/servicios/cliente.py
@@ -3,6 +3,7 @@ Implementación de un cliente de Rest con la librería requests
 """
 
 import requests
+import json
 
 
 def testHelloWorld():
@@ -10,5 +11,16 @@ def testHelloWorld():
     resp = requests.get(url)
     print(resp.json())
 
-if __name__ == '__main__':
-    testHelloWorld()
+
+def testProductosGet():
+    url = "http://localhost:5000/productos"
+    resp = requests.get(url)
+    fich = open("productos.json", "w")
+    json.dump(resp.json(), fich)
+    fich.close()
+    print("Se ha descargado el fichero: productos.json")
+
+
+if __name__ == "__main__":
+    #testHelloWorld()
+    testProductosGet()
