2b9a48778c990c29d8c41e350a66f815d6f22ae7
aldebarran22 <antonioespinherranz@hotmail.com>
2024-12-20 12:29:28 +0100

    12.29

---
+++ codigo2/servicios/servicioSOAP.py
@@ -0,0 +1,34 @@
+"""
+Implementación de un Servicio SOAP con la librería pysimplesoap
+"""
+
+from pysimplesoap import SoapDispatcher, SOAPHandler
+from http.server import HTTPServer
+
+
+def hello(name):
+    return {"mensaje": f"Hello {name}"}
+
+
+# Crear el dispatcher:
+dispatcher = SoapDispatcher(
+    "mi_dispatcher",
+    location="http://localhost:8008/",
+    action="http://localhost:8008/",
+    namespace="http://example.com/sample.wsdl",
+    prefix="ns0",
+    trace=True,
+    ns=True,
+)
+
+# Registrar la función
+dispatcher.register_function(
+    "Hello", hello, returns={"mensaje": str}, args={"name": str}
+)
+
+# Poner en marcha el servidor:
+if __name__ == "__main__":
+    print("Servidor Ok!")
+    server = HTTPServer(("", 8008), SOAPHandler)
+    server.dispatcher = dispatcher
+    server.serve_forever()
