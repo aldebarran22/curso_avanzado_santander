6a84a8455aafa286e3ed778b5ca5933ad7a95dba
aldebarran22 <antonioespinherranz@hotmail.com>
2024-12-20 13:06:09 +0100

    13.06

--- codigo2/mensajes_loggin.py
+++ codigo2/mensajes_loggin.py
@@ -12,6 +12,14 @@ def prueba1():
     logging.error('mensaje de error')
     logging.critical('mensaje de critical')
 
+def prueba2():
+    logging.basicConfig(filename="mensajes.log", level=logging.DEBUG)
+    logging.debug('mensaje de debug')
+    logging.info('mensaje de info')
+    logging.warning('mensaje de warning')
+    logging.error('mensaje de error')
+    logging.critical('mensaje de critical')
+
 
 if __name__ == "__main__":
     prueba1()
